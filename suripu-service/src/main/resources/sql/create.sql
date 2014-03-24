095cc8b0680288c45904a65058ff424e49be18a3
Tim Bart <tim@sayhello.com>
2014-03-24 10:24:44 -0700

    Updating documentation for SQL table

--- suripu-service/src/main/resources/sql/create.sql
+++ suripu-service/src/main/resources/sql/create.sql
@@ -15,3 +15,18 @@ GRANT ALL PRIVILEGES ON sensor_samples TO ingress_user;
 
 CREATE ROLE ingress_user WITH LOGIN ENCRYPTED PASSWORD 'hello ingress user' CREATEDB;
 ALTER ROLE ingress_user REPLICATION;
+
+
+
+--CREATE TABLE sensor_samples (
+--    id BIGSERIAL PRIMARY KEY,
+--    device_id BIGINT,
+--    sensor_id INT,
+--    ts TIMESTAMP,
+--    val INT
+--);
+--
+--CREATE UNIQUE INDEX uniq_sample on sensor_samples(device_id, ts);
+--
+--GRANT ALL PRIVILEGES ON sensor_samples TO ingress_user;
+--GRANT ALL PRIVILEGES ON sensor_samples TO ingress_user;
