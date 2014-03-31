07a193230c3d2a9bff43a78ed05c38b1151df970
Tim Bart <tim@sayhello.com>
2014-03-31 14:20:24 -0700

    Helpers for Application store from Postgres

---
+++ suripu-core/src/main/java/com/hello/suripu/core/db/ApplicationMapper.java
@@ -0,0 +1,37 @@
+package com.hello.suripu.core.db;
+
+import com.hello.suripu.core.oauth.Application;
+import com.hello.suripu.core.oauth.OAuthScope;
+import org.joda.time.DateTime;
+import org.skife.jdbi.v2.StatementContext;
+import org.skife.jdbi.v2.tweak.ResultSetMapper;
+
+import java.sql.Array;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ApplicationMapper implements ResultSetMapper<Application>{
+    @Override
+    public Application map(int index, ResultSet r, StatementContext ctx) throws SQLException {
+
+        final Array scopes = r.getArray("scopes");
+        // TODO: Scopes is nullable, handle failure cases
+        final Integer[] a = (Integer[]) scopes.getArray();
+        final OAuthScope[] s = new OAuthScope[a.length];
+        for(int i = 0; i < a.length; i ++) {
+            s[i] = OAuthScope.values()[a[i]];
+        }
+        return new Application(
+                r.getLong("id"),
+                r.getString("name"),
+                r.getString("client_id"),
+                r.getString("client_secret"),
+                r.getString("redirect_uri"),
+                s,
+                r.getLong("dev_account_id"),
+                r.getString("description"),
+                r.getBoolean("published"),
+                new DateTime(r.getTimestamp("created"))
+        );
+    }
+}
---
+++ suripu-core/src/main/java/com/hello/suripu/core/db/ApplicationsDAO.java
@@ -0,0 +1,39 @@
+package com.hello.suripu.core.db;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
+import com.hello.suripu.core.oauth.Application;
+import com.hello.suripu.core.oauth.ApplicationRegistration;
+import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.GetGeneratedKeys;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
+import org.skife.jdbi.v2.sqlobject.customizers.SingleValueResult;
+
+@RegisterMapper(ApplicationMapper.class)
+public interface ApplicationsDAO {
+
+    @GetGeneratedKeys
+    @SqlUpdate("INSERT INTO oauth_applications (name, client_id, client_secret, redirect_uri, scopes, dev_account_id, description) " +
+            "VALUES(:name, :client_id, :client_secret, :redirect_uri, :scopes, :dev_account_id, :description);")
+    Long insertRegistration(@BindApplicationRegistration ApplicationRegistration applicationRegistration);
+
+    @SqlQuery("SELECT * FROM oauth_applications WHERE id = :id")
+    @SingleValueResult(Application.class)
+    Optional<Application> getById(@Bind("id") Long applicationId);
+
+    @SqlQuery("SELECT * FROM oauth_applications WHERE client_id = :client_id")
+    @RegisterMapper(ApplicationMapper.class)
+    @SingleValueResult(Application.class)
+    Optional<Application> getByClientId(@Bind("client_id") String applicationClientId);
+
+    @SqlQuery("SELECT * FROM oauth_applications WHERE dev_account_id = :dev_account_id")
+    ImmutableList<Application> getAllByDevId(@Bind("dev_account_id") Long devAccountId);
+
+    @SqlQuery("SELECT count(*) FROM authorized_oauth_applications WHERE app_id = :app_id AND account_id = : account_id LIMIT 1;")
+    int exists(@Bind("app_id") Long appId, @Bind("account_id") Long accountId);
+
+    @SqlUpdate("INSERT INTO authorized_oauth_applications (app_id, account_id) VALUES(:app_id, :account_id);")
+    void insertInstallation(@Bind("app_id") Long appId, @Bind("account_id") Long accountId);
+}
