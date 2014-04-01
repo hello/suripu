3253ffcff1d83cb44403efc59a4583e1b173ec72
Tim Bart <tim@sayhello.com>
2014-04-01 16:04:02 -0700

    Cleaning up imports

--- suripu-app/src/main/java/com/hello/suripu/app/resources/ApplicationResource.java
+++ suripu-app/src/main/java/com/hello/suripu/app/resources/ApplicationResource.java
@@ -1,18 +1,19 @@
 package com.hello.suripu.app.resources;
 
-import com.google.common.collect.ImmutableList;
-import com.hello.suripu.core.oauth.*;
+import com.hello.suripu.core.oauth.Application;
+import com.hello.suripu.core.oauth.ApplicationRegistration;
+import com.hello.suripu.core.oauth.ApplicationStore;
+import com.hello.suripu.core.oauth.OAuthScope;
 
 import javax.validation.Valid;
-import javax.ws.rs.POST;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
 import java.util.List;
 
 @Path("applications")
