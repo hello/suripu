f035ddedf665591ab2bd3054d22a00ffcdacaef2
Tim Bart <tim@sayhello.com>
2014-04-02 11:27:12 -0700

    Switching to Optional for retrieving device_id from db

--- suripu-core/src/main/java/com/hello/suripu/core/db/DeviceDAO.java
+++ suripu-core/src/main/java/com/hello/suripu/core/db/DeviceDAO.java
@@ -1,10 +1,13 @@
 package com.hello.suripu.core.db;
 
+import com.google.common.base.Optional;
 import org.skife.jdbi.v2.sqlobject.Bind;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.customizers.SingleValueResult;
 
 public interface DeviceDAO {
 
+    @SingleValueResult(Long.class)
     @SqlQuery("SELECT id FROM account_device_map WHERE device_id = :device_id;")
-    Long getDeviceForAccountId(@Bind("device_id") String deviceId);
+    Optional<Long> getDeviceForAccountId(@Bind("device_id") String deviceId);
 }
