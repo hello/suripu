e3f3fc2069e00ace5b3928a9df16677768da6117
Tim Bart <tim@sayhello.com>
2014-04-15 15:23:38 -0700

    Score DB wrapper, binder, mapper

---
+++ suripu-core/src/main/java/com/hello/suripu/core/db/ScoreDAO.java
@@ -0,0 +1,21 @@
+package com.hello.suripu.core.db;
+
+import com.google.common.collect.ImmutableList;
+import com.hello.suripu.core.Score;
+import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
+
+
+@RegisterMapper(ScoreMapper.class)
+public interface ScoreDAO {
+
+    @SqlQuery("SELECT * FROM account_scores WHERE account_id = : account_id ORDER BY id desc LIMIT 7;")
+    ImmutableList<Score> getRecentScores(@Bind("account_id") Long accountId);
+
+
+    @SqlUpdate("INSERT INTO account_scores (account_id, ambient_temp, ambient_humidity, ambient_air_quality, ambient_light, ts, offset_millis) VALUES " +
+            "(:account_id, :ambient_temp, :ambient_humidity, :ambient_air_quality, :ambient_light, :ts, :offset_millis);")
+    void insertScore(@BindScore Score score);
+}
