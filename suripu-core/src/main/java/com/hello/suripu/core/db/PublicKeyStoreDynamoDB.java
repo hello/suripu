653cf8a1a23143275ce37ac259996f9b155a97fb
Tim Bart <tim@sayhello.com>
2014-04-29 15:14:09 -0700

    Using constants for dynamoDB attribute and removing unused code

--- suripu-core/src/main/java/com/hello/suripu/core/db/PublicKeyStoreDynamoDB.java
+++ suripu-core/src/main/java/com/hello/suripu/core/db/PublicKeyStoreDynamoDB.java
@@ -15,6 +15,9 @@ public class PublicKeyStoreDynamoDB implements PublicKeyStore {
     private final AmazonDynamoDBClient dynamoDBClient;
     private final String keyStoreTableName;
 
+    private final static String DEVICE_ID_ATTRIBUTE_NAME = "device_id";
+    private final static String PUBLIC_KEY_ATTRIBUTE_NAME = "public_key";
+
     public PublicKeyStoreDynamoDB(
             final AmazonDynamoDBClient dynamoDBClient,
             final String keyStoreTableName) {
@@ -24,25 +27,22 @@ public class PublicKeyStoreDynamoDB implements PublicKeyStore {
 
     @Override
     public byte[] get(final String deviceId) {
-        final Map<String, AttributeValue> attributes = new HashMap<String, AttributeValue>();
-        attributes.put("public_key", new AttributeValue().withS(""));
-
         final HashMap<String, AttributeValue> key = new HashMap<String, AttributeValue>();
-        key.put("device_id", new AttributeValue().withS(deviceId));
+        key.put(DEVICE_ID_ATTRIBUTE_NAME, new AttributeValue().withS(deviceId));
         final GetItemRequest getItemRequest = new GetItemRequest()
                 .withTableName(keyStoreTableName)
                 .withKey(key);
 
         final GetItemResult getItemResult = dynamoDBClient.getItem(getItemRequest);
-        final String base64EncodedPublicKey = getItemResult.getItem().get("public_key").getS();
+        final String base64EncodedPublicKey = getItemResult.getItem().get(PUBLIC_KEY_ATTRIBUTE_NAME).getS();
         return base64EncodedPublicKey.getBytes();
     }
 
     @Override
     public void put(final String deviceId, final String publicKey) {
         final Map<String, AttributeValue> attributes = new HashMap<String, AttributeValue>();
-        attributes.put("device_id", new AttributeValue().withS(deviceId));
-        attributes.put("public_key", new AttributeValue().withS(publicKey));
+        attributes.put(DEVICE_ID_ATTRIBUTE_NAME, new AttributeValue().withS(deviceId));
+        attributes.put(PUBLIC_KEY_ATTRIBUTE_NAME, new AttributeValue().withS(publicKey));
 
 //        final HashMap<String, AttributeValue> key = new HashMap<String, AttributeValue>();
 //        key.put("device_id", new AttributeValue().withS(deviceId));
