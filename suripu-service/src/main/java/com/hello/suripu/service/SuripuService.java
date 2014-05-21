b1f40fbe904fcc7a72fbe185cfec40f16369e5a8
Tim Bart <tim@sayhello.com>
2014-05-21 14:49:35 -0700

    Add new healthcheck and refactor credentials logic

--- suripu-service/src/main/java/com/hello/suripu/service/SuripuService.java
+++ suripu-service/src/main/java/com/hello/suripu/service/SuripuService.java
@@ -1,7 +1,6 @@
 package com.hello.suripu.service;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.kinesis.AmazonKinesisAsyncClient;
 import com.hello.dropwizard.mikkusu.helpers.JacksonProtobufProvider;
@@ -9,6 +8,7 @@ import com.hello.dropwizard.mikkusu.resources.PingResource;
 import com.hello.dropwizard.mikkusu.resources.VersionResource;
 import com.hello.suripu.core.JodaArgumentFactory;
 import com.hello.suripu.core.KinesisLogger;
+import com.hello.suripu.core.aws.AWSHelper;
 import com.hello.suripu.core.db.AccessTokenDAO;
 import com.hello.suripu.core.db.ApplicationsDAO;
 import com.hello.suripu.core.db.DeviceDAO;
@@ -18,7 +18,9 @@ import com.hello.suripu.core.db.PublicKeyStoreDynamoDB;
 import com.hello.suripu.core.db.ScoreDAO;
 import com.hello.suripu.core.db.TrackerMotionDAO;
 import com.hello.suripu.core.health.DynamoDbHealthCheck;
+import com.hello.suripu.core.health.KinesisHealthCheck;
 import com.hello.suripu.core.managers.DynamoDBClientManaged;
+import com.hello.suripu.core.managers.KinesisClientManaged;
 import com.hello.suripu.core.oauth.AccessToken;
 import com.hello.suripu.core.oauth.ClientCredentials;
 import com.hello.suripu.core.oauth.ClientDetails;
@@ -36,8 +38,12 @@ import com.yammer.dropwizard.config.Environment;
 import com.yammer.dropwizard.jdbi.DBIFactory;
 import com.yammer.dropwizard.jdbi.bundles.DBIExceptionsBundle;
 import org.skife.jdbi.v2.DBI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SuripuService extends Service<SuripuConfiguration> {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(SuripuService.class);
 
     public static void main(String[] args) throws Exception {
         new SuripuService().run(args);
@@ -64,7 +70,9 @@ public class SuripuService extends Service<SuripuConfiguration> {
         final TrackerMotionDAO trackerMotionDAO = jdbi.onDemand(TrackerMotionDAO.class);
         final EventDAO eventDAO = jdbi.onDemand(EventDAO.class);
 
-        final AWSCredentialsProvider awsCredentialsProvider = new EnvironmentVariableCredentialsProvider();
+
+        final AWSCredentialsProvider awsCredentialsProvider = AWSHelper.getCredentials();
+
         final AmazonDynamoDBClient dynamoDBClient = new AmazonDynamoDBClient(awsCredentialsProvider);
 
         final AmazonKinesisAsyncClient kinesisClient = new AmazonKinesisAsyncClient(awsCredentialsProvider);
@@ -89,9 +97,15 @@ public class SuripuService extends Service<SuripuConfiguration> {
         environment.addResource(new ReceiveResource(dao, deviceDAO, scoreDAO, trackerMotionDAO, publicKeyStore, kinesisLogger));
         environment.addResource(new PingResource());
         environment.addResource(new VersionResource());
+
+        // Manage the lifecycle of our clients
         environment.manage(new DynamoDBClientManaged(dynamoDBClient));
+        environment.manage(new KinesisClientManaged(kinesisClient));
+
+        // Make sure we can connect
         environment.addHealthCheck(new DynamoDbHealthCheck(dynamoDBClient));
-
-
+        environment.addHealthCheck(new KinesisHealthCheck(kinesisClient));
     }
+
+
 }
