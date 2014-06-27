c9054029eb97330b98798a8513bfb3ddc5b8c60b
Tim Bart <tim@sayhello.com>
2014-06-27 14:53:22 -0700

    Updating TrackerMotionDAO and its usage

--- suripu-core/src/main/java/com/hello/suripu/core/models/DeviceAccountPair.java
+++ suripu-core/src/main/java/com/hello/suripu/core/models/DeviceAccountPair.java
@@ -4,9 +4,11 @@ public class DeviceAccountPair {
 
     public final Long accountId;
     public final Long internalDeviceId;
+    public final String externalDeviceId;
 
-    public DeviceAccountPair(final Long accountId, final Long internalDeviceId) {
+    public DeviceAccountPair(final Long accountId, final Long internalDeviceId, final String externalDeviceId) {
         this.accountId = accountId;
         this.internalDeviceId = internalDeviceId;
+        this.externalDeviceId = externalDeviceId;
     }
 }
