8de12ad8cd83790df82e111ff29f24dfacb4e5f5
Tim Bart <tim@sayhello.com>
2014-08-04 16:21:00 -0700

    Adding config deploy script

---
+++ configs.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+VERSION=$1
+
+s3cmd put suripu-app/suripu-app.prod.yml s3://hello-deploy/configs/com/hello/suripu/suripu-app/$VERSION/suripu-app.prod.yml
+s3cmd put suripu-service/suripu-service.prod.yml s3://hello-deploy/configs/com/hello/suripu/suripu-service/$VERSION/suripu-service.prod.yml
