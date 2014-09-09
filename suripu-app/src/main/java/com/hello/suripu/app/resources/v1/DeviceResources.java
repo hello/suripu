0c110bdf2d447dd327d86b4e57ec6cdb24b564d6
Tim Bart <tim@sayhello.com>
2014-09-09 14:09:47 -0700

    Versioning API endpoints /v1

---
+++ suripu-app/src/main/java/com/hello/suripu/app/resources/v1/DeviceResources.java
@@ -0,0 +1,55 @@
+package com.hello.suripu.app.resources.v1;
+
+import com.hello.suripu.core.db.DeviceDAO;
+import com.hello.suripu.core.db.util.MatcherPatternsDB;
+import com.hello.suripu.core.models.PillRegistration;
+import com.hello.suripu.core.oauth.AccessToken;
+import com.hello.suripu.core.oauth.OAuthScope;
+import com.hello.suripu.core.oauth.Scope;
+import com.hello.suripu.core.util.JsonError;
+import org.skife.jdbi.v2.exceptions.UnableToExecuteStatementException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.validation.Valid;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.regex.Matcher;
+
+@Path("/v1/devices")
+public class DeviceResources {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DeviceResources.class);
+    private final DeviceDAO deviceDAO;
+
+    public DeviceResources(final DeviceDAO deviceDAO) {
+        this.deviceDAO = deviceDAO;
+    }
+
+    @POST
+    @Path("/pill")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public void registerPill(@Scope(OAuthScope.ADMINISTRATION_WRITE) final AccessToken accessToken, @Valid final PillRegistration pillRegistration) {
+        try {
+            final Integer trackerId = deviceDAO.registerTracker(accessToken.accountId, pillRegistration.pillId);
+            LOGGER.info("Account {} registered pill {} with internal id = {}", accessToken.accountId, pillRegistration.pillId, trackerId);
+            return;
+        } catch (UnableToExecuteStatementException exception) {
+            final Matcher matcher = MatcherPatternsDB.PG_UNIQ_PATTERN.matcher(exception.getMessage());
+
+            if(matcher.find()) {
+                LOGGER.error("Failed to register pill for account id = {} and pill id = {} : {}", accessToken.accountId, pillRegistration.pillId, exception.getMessage());
+                throw new WebApplicationException(Response.status(Response.Status.CONFLICT)
+                        .entity(new JsonError(409, "Pill already exists for this account.")).build());
+            }
+        }
+
+        throw new WebApplicationException(Response.Status.BAD_REQUEST);
+    }
+}
