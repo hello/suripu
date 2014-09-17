3721cc8926c50d1505cc4da19b37dacc8125008d
Pang Wu <pang@sayhello.com>
2014-09-17 16:47:02 -0700

    Add test dir back

---
+++ suripu-app/src/test/java/com/hello/suripu/app/resources/v1/AlarmResourceTest.java
@@ -0,0 +1,166 @@
+package com.hello.suripu.app.resources.v1;
+
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.model.DeleteTableRequest;
+import com.amazonaws.services.dynamodbv2.model.ResourceInUseException;
+import com.amazonaws.services.dynamodbv2.model.ResourceNotFoundException;
+import com.hello.suripu.core.db.AlarmDAODynamoDB;
+import com.hello.suripu.core.models.Alarm;
+import com.hello.suripu.core.models.AlarmSound;
+import com.hello.suripu.core.oauth.AccessToken;
+import org.joda.time.DateTime;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+
+/**
+ * Created by pangwu on 9/17/14.
+ */
+public class AlarmResourceTest {
+
+
+    private BasicAWSCredentials awsCredentials;
+    private AmazonDynamoDBClient amazonDynamoDBClient;
+    private AlarmDAODynamoDB alarmDAODynamoDB;
+
+    private AlarmResource alarmResource;
+    private final String tableName = "alarm_test";
+
+
+    private final List<Alarm> validList = new ArrayList<Alarm>();
+    private final List<Alarm> tooMuchAlarmList = new ArrayList<Alarm>();
+    private final List<Alarm> twoAlarmInADayList = new ArrayList<Alarm>();
+
+    private final AccessToken token = new AccessToken.Builder().withAccountId(1L).build();
+
+
+    @Before
+    public void setUp(){
+        this.awsCredentials = new BasicAWSCredentials("FAKE_AWS_KEY", "FAKE_AWS_SECRET");
+        ClientConfiguration clientConfiguration = new ClientConfiguration();
+        clientConfiguration.setMaxErrorRetry(0);
+        this.amazonDynamoDBClient = new AmazonDynamoDBClient(this.awsCredentials, clientConfiguration);
+        this.amazonDynamoDBClient.setEndpoint("http://localhost:7777");
+
+        cleanUp();
+
+        try {
+            AlarmDAODynamoDB.createTable(tableName, this.amazonDynamoDBClient);
+            this.alarmDAODynamoDB = new AlarmDAODynamoDB(
+                    this.amazonDynamoDBClient,
+                    tableName
+            );
+
+            this.alarmResource = new AlarmResource(this.alarmDAODynamoDB);
+
+
+        }catch (ResourceInUseException rie){
+            rie.printStackTrace();
+        }
+
+
+        // Create an alarm list that contains valid alarms
+        final Alarm.Builder builder = new Alarm.Builder();
+        final DateTime now = DateTime.now();
+
+        for(int i = 0; i < 7; i++){
+            final DateTime targetDate = now.plusDays(i);
+            final HashSet<Integer> dayOfWeek = new HashSet<Integer>();
+            dayOfWeek.add(targetDate.getDayOfWeek());
+
+            builder.withYear(targetDate.getYear())
+                    .withMonth(targetDate.getMonthOfYear())
+                    .withDay(targetDate.getDayOfMonth())
+                    .withDayOfWeek(dayOfWeek)
+                    .withHour(targetDate.getHourOfDay())
+                    .withMinute(targetDate.getMinuteOfHour())
+                    .withIsRepeated(false)
+                    .withIsEnabled(true)
+                    .withIsEditable(true)
+                    .withAlarmSound(new AlarmSound(1, "La Marseillaise"));
+
+            this.validList.add(builder.build());
+
+        }
+
+
+        // Create an alarm list that contains 8 alarms
+        for(int i = 0; i < 8; i++){
+            final DateTime targetDate = now.plusDays(i);
+            final HashSet<Integer> dayOfWeek = new HashSet<Integer>();
+            dayOfWeek.add(targetDate.getDayOfWeek());
+
+            builder.withYear(targetDate.getYear())
+                    .withMonth(targetDate.getMonthOfYear())
+                    .withDay(targetDate.getDayOfMonth())
+                    .withDayOfWeek(dayOfWeek)
+                    .withHour(targetDate.getHourOfDay())
+                    .withMinute(targetDate.getMinuteOfHour())
+                    .withIsRepeated(false)
+                    .withIsEnabled(true)
+                    .withIsEditable(true)
+                    .withAlarmSound(new AlarmSound(1, "La Marseillaise"));
+
+            this.tooMuchAlarmList.add(builder.build());
+
+        }
+
+
+        // Create an alarm list that contains 2 alarms in the same day
+        for(int i = 0; i < 2; i++){
+            final DateTime targetDate = now.withTimeAtStartOfDay().plusHours(i);
+            final HashSet<Integer> dayOfWeek = new HashSet<Integer>();
+            dayOfWeek.add(targetDate.getDayOfWeek());
+
+            builder.withYear(targetDate.getYear())
+                    .withMonth(targetDate.getMonthOfYear())
+                    .withDay(targetDate.getDayOfMonth())
+                    .withDayOfWeek(dayOfWeek)
+                    .withHour(targetDate.getHourOfDay())
+                    .withMinute(targetDate.getMinuteOfHour())
+                    .withIsRepeated(false)
+                    .withIsEnabled(true)
+                    .withIsEditable(true)
+                    .withAlarmSound(new AlarmSound(1, "La Marseillaise"));
+
+            this.twoAlarmInADayList.add(builder.build());
+
+        }
+    }
+
+
+    @After
+    public void cleanUp(){
+        final DeleteTableRequest deleteTableRequest = new DeleteTableRequest()
+                .withTableName(tableName);
+        try {
+            this.amazonDynamoDBClient.deleteTable(deleteTableRequest);
+        }catch (ResourceNotFoundException ex){
+            ex.printStackTrace();
+        }
+
+        this.validList.clear();
+        this.tooMuchAlarmList.clear();
+        this.twoAlarmInADayList.clear();
+    }
+
+
+    @Test
+    public void testSetAndGetValidAlarm(){
+        this.alarmResource.setAlarms(this.token, DateTime.now().getMillis(), this.validList);
+        final List<Alarm> actual = this.alarmResource.getAlarms(this.token);
+        final List<Alarm> expected = this.validList;
+
+        assertThat(actual, containsInAnyOrder(expected.toArray()));
+    }
+
+}
