184f59f5b893568ec69268cd116b59cd8c2014af
Pang Wu <pang@sayhello.com>
2014-09-16 17:57:47 -0700

    Initial pass on alarm storage

---
+++ suripu-core/src/main/java/com/hello/suripu/core/models/Alarm.java
@@ -0,0 +1,75 @@
+package com.hello.suripu.core.models;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Objects;
+
+import java.util.Arrays;
+
+/**
+ * Created by pangwu on 9/16/14.
+ */
+public class Alarm {
+
+    @JsonProperty("year")
+    public final int year;
+
+    @JsonProperty("month")
+    public final int month;
+
+    @JsonProperty("day_of_month")
+    public final int day;
+
+    @JsonProperty("hour")
+    public final int hourOfDay;
+
+    @JsonProperty("minute")
+    public final int minuteOfHour;
+
+    @JsonProperty("repeated")
+    public final boolean isRepeated;
+
+    @JsonProperty("day_of_week")
+    public final int[] dayOfWeek;
+
+    @JsonProperty("sound_id")
+    public final long soundId;
+
+    @JsonCreator
+    public Alarm(@JsonProperty("year") int year,
+                 @JsonProperty("month") int month,
+                 @JsonProperty("day_of_month") int day,
+                 @JsonProperty("hour")int hourOfDay,
+                 @JsonProperty("minute") int minuteOfHour,
+                 @JsonProperty("days") int[] dayOfWeek,
+                 @JsonProperty("repeated") boolean isRepeated,
+                 @JsonProperty("sound_id") long soundId){
+        this.dayOfWeek = dayOfWeek;
+        this.hourOfDay = hourOfDay;
+        this.minuteOfHour = minuteOfHour;
+        this.isRepeated = isRepeated;
+        this.soundId = soundId;
+
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    @Override
+    public boolean equals(final Object other){
+        if (other == null){
+            return false;
+        }
+
+        if (getClass() != other.getClass()){
+            return false;
+        }
+
+        final Alarm convertedObject = (Alarm) other;
+        return Objects.equal(convertedObject.hourOfDay, this.hourOfDay) &&
+                Objects.equal(convertedObject.minuteOfHour, this.minuteOfHour) &&
+                Objects.equal(convertedObject.isRepeated, this.isRepeated) &&
+                Objects.equal(convertedObject.soundId, this.soundId) &&
+                Arrays.equals(convertedObject.dayOfWeek, this.dayOfWeek);
+    }
+}
---
+++ suripu-core/src/test/java/com/hello/suripu/core/db/AlarmDAODynamoDBTest.java
@@ -0,0 +1,120 @@
+package com.hello.suripu.core.db;
+
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.model.DeleteTableRequest;
+import com.amazonaws.services.dynamodbv2.model.ResourceInUseException;
+import com.amazonaws.services.dynamodbv2.model.ResourceNotFoundException;
+import com.google.common.collect.ImmutableList;
+import com.hello.suripu.core.models.Alarm;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeConstants;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.core.Is.is;
+
+
+/**
+ * Created by pangwu on 9/16/14.
+ */
+public class AlarmDAODynamoDBTest {
+
+    private BasicAWSCredentials awsCredentials;
+    private AmazonDynamoDBClient amazonDynamoDBClient;
+    private AlarmDAODynamoDB alarmDAODynamoDB;
+    private final String tableName = "alarm_test";
+
+    @Before
+    public void setUp(){
+        this.awsCredentials = new BasicAWSCredentials("FAKE_AWS_KEY", "FAKE_AWS_SECRET");
+        ClientConfiguration clientConfiguration = new ClientConfiguration();
+        clientConfiguration.setMaxErrorRetry(0);
+        this.amazonDynamoDBClient = new AmazonDynamoDBClient(this.awsCredentials, clientConfiguration);
+        this.amazonDynamoDBClient.setEndpoint("http://localhost:7777");
+
+        cleanUp();
+
+        try {
+            AlarmDAODynamoDB.createTable(tableName, this.amazonDynamoDBClient);
+            this.alarmDAODynamoDB = new AlarmDAODynamoDB(
+                    this.amazonDynamoDBClient,
+                    tableName
+            );
+
+
+        }catch (ResourceInUseException rie){
+            rie.printStackTrace();
+        }
+    }
+
+
+    @After
+    public void cleanUp(){
+        final DeleteTableRequest deleteTableRequest = new DeleteTableRequest()
+                .withTableName(tableName);
+        try {
+            this.amazonDynamoDBClient.deleteTable(deleteTableRequest);
+        }catch (ResourceNotFoundException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testGetEmptyAlarmList(){
+        long accountId = 1;
+        final ImmutableList<Alarm> actual = this.alarmDAODynamoDB.getAlarms(accountId);
+        final ImmutableList<Alarm> expected = ImmutableList.copyOf(Collections.EMPTY_LIST);
+        assertThat(expected, containsInAnyOrder(actual.toArray()));
+        assertThat(expected.size(), is(actual.size()));
+    }
+
+    @Test
+    public void testSetEmptyAlarmList(){
+        long accountId = 1;
+        this.alarmDAODynamoDB.setAlarms(accountId, new ArrayList<Alarm>());
+        final ImmutableList<Alarm> expected = ImmutableList.copyOf(Collections.EMPTY_LIST);
+
+        final ImmutableList<Alarm> actual = this.alarmDAODynamoDB.getAlarms(accountId);
+        assertThat(expected, containsInAnyOrder(actual.toArray()));
+        assertThat(expected.size(), is(actual.size()));
+    }
+
+
+    @Test
+    public void testSetValidAlarmList(){
+        long accountId = 1;
+        final List<Alarm> expected = new ArrayList<Alarm>();
+
+        final DateTime now = DateTime.now();
+        expected.add(new Alarm(0, 0, 0, 0, 1, new int[]{ DateTimeConstants.MONDAY}, true, 0));
+        expected.add(new Alarm(now.getYear(), now.getMonthOfYear(), now.getDayOfMonth(), 1, 1, new int[0], false, 1));
+
+        this.alarmDAODynamoDB.setAlarms(accountId, expected);
+        final ImmutableList<Alarm> actual = this.alarmDAODynamoDB.getAlarms(accountId);
+        assertThat(expected, containsInAnyOrder(actual.toArray()));
+        assertThat(actual.size(), is(expected.size()));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testSetTooMuchAlarms(){
+        long accountId = 1;
+        final List<Alarm> expected = new ArrayList<Alarm>();
+
+        for(int i = 0; i < AlarmDAODynamoDB.MAX_ALARM_COUNT + 1; i++) {
+            expected.add(new Alarm(0, 0, 0, 0, i + 1, new int[]{DateTimeConstants.MONDAY}, true, 0));
+        }
+
+        this.alarmDAODynamoDB.setAlarms(accountId, expected);
+    }
+
+
+}
