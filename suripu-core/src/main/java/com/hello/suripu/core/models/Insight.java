988aa04bb1468342521757fe7218f7a1899b2e7f
Tim Bart <tim@sayhello.com>
2014-10-14 15:20:16 -0700

    Making JSON property names explicit for Insight

--- suripu-core/src/main/java/com/hello/suripu/core/models/Insight.java
+++ suripu-core/src/main/java/com/hello/suripu/core/models/Insight.java
@@ -1,9 +1,16 @@
 package com.hello.suripu.core.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Insight {
 
+    @JsonProperty("condition")
     final public CurrentRoomState.State.Condition condition;
+
+    @JsonProperty("message")
     final public String message;
+
+    @JsonProperty("sensor")
     final public Sensor sensor;
 
     public Insight(final Sensor sensor, final CurrentRoomState.State.Condition condition, final String message) {
