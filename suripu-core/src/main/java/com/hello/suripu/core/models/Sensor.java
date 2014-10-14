5d67677f67a99248b4fb0b5131d59004f22eccbd
Tim Bart <tim@sayhello.com>
2014-10-14 14:31:29 -0700

    Make sensor name lowercase

--- suripu-core/src/main/java/com/hello/suripu/core/models/Sensor.java
+++ suripu-core/src/main/java/com/hello/suripu/core/models/Sensor.java
@@ -1,5 +1,7 @@
 package com.hello.suripu.core.models;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Sensor {
 
     TEMPERATURE("temperature"),
@@ -13,5 +15,11 @@ public enum Sensor {
 
     private Sensor(String value) {
         this.value = value;
+    }
+
+
+    @JsonValue
+    public String toString() {
+        return value;
     }
 }
