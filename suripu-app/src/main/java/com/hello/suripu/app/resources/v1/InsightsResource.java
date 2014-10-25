32be5263f16f792b39fe5e64925fb18190dda1bd
kingshy <kingshy@sayhello.com>
2014-10-24 17:35:18 -0700

    rm account-id check, authenticator will do it

--- suripu-app/src/main/java/com/hello/suripu/app/resources/v1/InsightsResource.java
+++ suripu-app/src/main/java/com/hello/suripu/app/resources/v1/InsightsResource.java
@@ -1,8 +1,6 @@
 package com.hello.suripu.app.resources.v1;
 
-import com.google.common.base.Optional;
 import com.hello.suripu.core.db.AccountDAO;
-import com.hello.suripu.core.models.Account;
 import com.hello.suripu.core.models.SleepInsight;
 import com.hello.suripu.core.oauth.AccessToken;
 import com.hello.suripu.core.oauth.OAuthScope;
@@ -15,7 +13,6 @@ import org.slf4j.LoggerFactory;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
@@ -39,10 +36,6 @@ public class InsightsResource {
             @Scope(OAuthScope.SLEEP_TIMELINE) final AccessToken accessToken) {
 
         LOGGER.debug("Returning list of insights for account id = {}", accessToken.accountId);
-        final Optional<Account> accountOptional = accountDAO.getById(accessToken.accountId);
-        if (!accountOptional.isPresent()) {
-            throw new WebApplicationException(404);
-        }
 
         return InsightProcessor.getInsights(accessToken.accountId);
     }
