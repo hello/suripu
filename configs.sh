1d2759bdf406bde531ddee8f0f73e3d865630827
Pang Wu <pang@sayhello.com>
2014-12-04 15:24:20 -0800

    Update upload script

--- configs.sh
+++ configs.sh
@@ -6,3 +6,4 @@ s3cmd put suripu-app/suripu-app.prod.yml s3://hello-deploy/configs/com/hello/sur
 s3cmd put suripu-service/suripu-service.prod.yml s3://hello-deploy/configs/com/hello/suripu/suripu-service/$VERSION/suripu-service.prod.yml
 s3cmd put suripu-workers/pillscorer.prod.yml s3://hello-deploy/configs/com/hello/suripu/suripu-workers/$VERSION/pillscorer.prod.yml
 s3cmd put suripu-workers/pill.prod.yml s3://hello-deploy/configs/com/hello/suripu/suripu-workers/$VERSION/pill.prod.yml
+s3cmd put suripu-workers/alarm_worker.prod.yml s3://hello-deploy/configs/com/hello/suripu/suripu-workers/$VERSION/alarm_worker.prod.yml
