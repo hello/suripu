fdfdb18f60380080d8d080400e876aa53734a372
longd3 <izzy3ds@gmail.com>
2014-12-31 20:49:26 -0800

    Fix typo in SenseUploadConfiguration

--- suripu-service/src/main/java/com/hello/suripu/service/configuration/SenseUploadConfiguration.java
+++ suripu-service/src/main/java/com/hello/suripu/service/configuration/SenseUploadConfiguration.java
@@ -3,8 +3,8 @@ package com.hello.suripu.service.configuration;
 
 public class SenseUploadConfiguration {
     private static final Integer DEFAULT_NON_PEAK_HOUR_LOWER_BOUND = 11;  // non peak periods start at 11:00:00
-    private static final Integer DEFAULT_NON_PEAK_HOUR_UPPER_BOUND = 20;  // non peak periods end at 20:00:59
-    private static final Boolean DEFAULT_WEEKS_DAY_ONLY = true;           // weekends are treated as peak periods
+    private static final Integer DEFAULT_NON_PEAK_HOUR_UPPER_BOUND = 20;  // non peak periods end at 20:59:59
+    private static final Boolean DEFAULT_WEEK_DAYS_ONLY = true;           // weekends are treated as peak periods
     private static final Integer DEFAULT_LONG_INTERVAL = 6;    // minutes
     private static final Integer DEFAULT_SHORT_INTERVAL = 2;   // minutes
 
@@ -27,6 +27,6 @@ public class SenseUploadConfiguration {
     }
 
     public SenseUploadConfiguration() {
-        this(DEFAULT_NON_PEAK_HOUR_LOWER_BOUND, DEFAULT_NON_PEAK_HOUR_UPPER_BOUND, DEFAULT_WEEKS_DAY_ONLY, DEFAULT_LONG_INTERVAL, DEFAULT_SHORT_INTERVAL);
+        this(DEFAULT_NON_PEAK_HOUR_LOWER_BOUND, DEFAULT_NON_PEAK_HOUR_UPPER_BOUND, DEFAULT_WEEK_DAYS_ONLY, DEFAULT_LONG_INTERVAL, DEFAULT_SHORT_INTERVAL);
     }
 }
