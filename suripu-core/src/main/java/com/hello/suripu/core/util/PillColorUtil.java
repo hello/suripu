bd9416f2c086352d86bbf0eed3ee319f7a3fc6f9
Pang Wu <pang@sayhello.com>
2015-01-09 10:32:10 -0800

    Swap color

--- suripu-core/src/main/java/com/hello/suripu/core/util/PillColorUtil.java
+++ suripu-core/src/main/java/com/hello/suripu/core/util/PillColorUtil.java
@@ -1,6 +1,6 @@
 package com.hello.suripu.core.util;
 
-import java.awt.*;
+import java.awt.Color;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.ArrayList;
@@ -11,8 +11,8 @@ import java.util.ArrayList;
 public class PillColorUtil {
     public static Color getPillColorByAccountRegistrationOrder(final int orderIndex){
         final ArrayList<Color> pillColors = new ArrayList<>();
-        pillColors.add(new Color(0xFE, 0x00, 0x00));
         pillColors.add(new Color(0x00, 0x00, 0xFE));
+        pillColors.add(new Color(0xFE, 0x00, 0x00));
         pillColors.add(new Color(0x00, 0xFE, 0xFE));
         pillColors.add(new Color(0xFE, 0xFE, 0x00));
 
