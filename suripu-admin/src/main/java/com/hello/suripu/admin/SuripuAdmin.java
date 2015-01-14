a09d13aae9e795fd0d8a13e8d2647f07e73eca50
longd3 <izzy3ds@gmail.com>
2015-01-13 17:28:14 -0800

    Configure suripu-admin to talk to common_db

--- suripu-admin/src/main/java/com/hello/suripu/admin/SuripuAdmin.java
+++ suripu-admin/src/main/java/com/hello/suripu/admin/SuripuAdmin.java
@@ -1,9 +1,14 @@
 package com.hello.suripu.admin;
 
 import com.hello.dropwizard.mikkusu.resources.PingResource;
+import com.hello.suripu.admin.resources.v1.AccountResource;
+import com.hello.suripu.core.db.AccountDAO;
+import com.hello.suripu.core.db.AccountDAOImpl;
 import com.yammer.dropwizard.Service;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
+import com.yammer.dropwizard.jdbi.DBIFactory;
+import org.skife.jdbi.v2.DBI;
 
 public class SuripuAdmin extends Service<SuripuAdminConfiguration> {
 
@@ -17,7 +22,11 @@ public class SuripuAdmin extends Service<SuripuAdminConfiguration> {
 
     @Override
     public void run(SuripuAdminConfiguration configuration, Environment environment) throws Exception {
+        final DBIFactory factory = new DBIFactory();
+        final DBI commonDB = factory.build(environment, configuration.getCommonDB(), "postgresql");
+        final AccountDAO accountDAO = commonDB.onDemand(AccountDAOImpl.class);
 
         environment.addResource(new PingResource());
+        environment.addResource(new AccountResource(accountDAO));
     }
 }
