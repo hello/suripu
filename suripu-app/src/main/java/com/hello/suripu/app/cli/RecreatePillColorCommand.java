6f07e13d230cd366a94cf5165da396aad9b4eb6d
Pang Wu <pang@sayhello.com>
2015-01-15 17:42:00 -0800

    Same pill shouldnt show different color

--- suripu-app/src/main/java/com/hello/suripu/app/cli/RecreatePillColorCommand.java
+++ suripu-app/src/main/java/com/hello/suripu/app/cli/RecreatePillColorCommand.java
@@ -8,7 +8,6 @@ import com.hello.suripu.core.db.DeviceDAO;
 import com.hello.suripu.core.db.MergedUserInfoDynamoDB;
 import com.hello.suripu.core.db.util.JodaArgumentFactory;
 import com.hello.suripu.core.models.DeviceAccountPair;
-import com.hello.suripu.core.models.UserInfo;
 import com.hello.suripu.core.util.PillColorUtil;
 import com.yammer.dropwizard.cli.ConfiguredCommand;
 import com.yammer.dropwizard.config.Bootstrap;
@@ -23,7 +22,7 @@ import org.skife.jdbi.v2.DBI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.awt.Color;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -61,9 +60,14 @@ public class RecreatePillColorCommand extends ConfiguredCommand<SuripuAppConfigu
         final String eventTableName = configuration.getUserInfoDynamoDBConfiguration().getTableName();
         final MergedUserInfoDynamoDB mergedUserInfoDynamoDB = new MergedUserInfoDynamoDB(client, eventTableName);
 
+        LOGGER.info("Getting all pills..");
         final List<DeviceAccountPair> activePills = deviceDAO.getAllPills(true);
+        LOGGER.info("Get pills list completed");
+
         final HashMap<String, ArrayList<DeviceAccountPair>> sensePillMap = new HashMap<>();
         for(final DeviceAccountPair pill:activePills){
+
+            LOGGER.info("Getting sense paired with account {}", pill.accountId);
             final List<DeviceAccountPair> sensePairedWithAccount = deviceDAO.getSensesForAccountId(pill.accountId);
             for(final DeviceAccountPair sense:sensePairedWithAccount){
                 if(!sensePillMap.containsKey(sense.externalDeviceId)){
@@ -71,10 +75,10 @@ public class RecreatePillColorCommand extends ConfiguredCommand<SuripuAppConfigu
                 }
                 sensePillMap.get(sense.externalDeviceId).add(pill);
             }
+            LOGGER.info("{} sense has linked with account {}", sensePairedWithAccount.size(), pill.accountId);
         }
 
         for(final String senseId:sensePillMap.keySet()){
-            final List<UserInfo> userInfoList = mergedUserInfoDynamoDB.getInfo(senseId);
 
             final ArrayList<DeviceAccountPair> pillsLinkedToSense = sensePillMap.get(senseId);
             Collections.sort(pillsLinkedToSense, new Comparator<DeviceAccountPair>() {
@@ -84,10 +88,22 @@ public class RecreatePillColorCommand extends ConfiguredCommand<SuripuAppConfigu
                 }
             });
 
+            LOGGER.info("Sense {} has {} accounts that has pill linked", senseId, pillsLinkedToSense.size());
+            final HashMap<String, Color> pillIdToColorMap = new HashMap<>();
             for(final DeviceAccountPair pill:pillsLinkedToSense){
-                final Color pillColor = PillColorUtil.getPillColorByAccountRegistrationOrder(userInfoList.size());
+                Color pillColor;
+
+                if(!pillIdToColorMap.containsKey(pill.externalDeviceId)) {
+                    pillColor = PillColorUtil.getPillColorByAccountRegistrationOrder(pillIdToColorMap.size());
+                    pillIdToColorMap.put(pill.externalDeviceId, new Color(pillColor.getRGB()));
+                }else{
+                    pillColor = pillIdToColorMap.get(pill.externalDeviceId);
+                }
                 mergedUserInfoDynamoDB.setPillColor(senseId, pill.accountId, pill.externalDeviceId, pillColor);
+                LOGGER.info("Pill {} linked with sense {} set to color {}", pill.externalDeviceId, senseId, pillColor.getRGB());
             }
         }
+
+        LOGGER.info("Reprocess done!");
     }
 }
