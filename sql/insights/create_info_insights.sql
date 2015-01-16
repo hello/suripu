9d015b6a4e7913c0508065acfacefd32491a365c
kingshy <kingshy@sayhello.com>
2015-01-16 15:05:51 -0800

    reformat info card

--- sql/insights/create_info_insights.sql
+++ sql/insights/create_info_insights.sql
@@ -36,14 +36,5 @@ GRANT ALL PRIVILEGES ON SEQUENCE info_insight_cards_id_seq TO ingress_user;
 INSERT INTO info_insight_cards (category, image_url, title, text)
 VALUES (2, 'https://s3.amazonaws.com/hello-data/insights_images/light_1.jpeg',
 'Effects of light in the environment on your sleep',
-'Light influences our internal clock through specialized "light sensitive" cells in the retina of our eyes,
-which sends messages to the brain that keep us in a 24-hour cycle or the **circadian rhythm**.
-These are physical, mental and behavioral changes that follow a roughly a 24-hour cycle,
-responding primarily to light and darkness in our environment.\n\nThe increase and decrease of light cues,
-trigger different chemical reactions our body, causing changes to our physiology and behavior. As evening
-approaches and the light in our environment recedes, the hormone melatonin begins to rise,
-the body temperature falls, thus making us less alert and more likely to fall sleep.
-In the morning light, melatonin levels are low, the body temperature begins to rise, and other
-chemical shifts, such as a rise in the activating hormone cortisol, then will help us to feel alert
-and ready for the day.'
+'Light influences our internal clock through specialized "light sensitive" cells in the retina of our eyes, which sends messages to the brain that keep us in a 24-hour cycle or the **circadian rhythm**. These are physical, mental and behavioral changes that follow a roughly a 24-hour cycle, responding primarily to light and darkness in our environment.\n\nThe increase and decrease of light cues, trigger different chemical reactions our body, causing changes to our physiology and behavior. As evening approaches and the light in our environment recedes, the hormone melatonin begins to rise, the body temperature falls, thus making us less alert and more likely to fall sleep. In the morning light, melatonin levels are low, the body temperature begins to rise, and other chemical shifts, such as a rise in the activating hormone cortisol, then will help us to feel alert and ready for the day.'
 );
