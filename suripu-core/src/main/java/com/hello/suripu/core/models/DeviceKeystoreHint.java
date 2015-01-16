974c493271cf5d106ab6d0fa0a454d6d74e2855a
longd3 <izzy3ds@gmail.com>
2015-01-16 12:42:11 -0800

    Add endpoint to view pill's key hints

--- suripu-core/src/main/java/com/hello/suripu/core/models/DeviceKeystoreHint.java
+++ suripu-core/src/main/java/com/hello/suripu/core/models/DeviceKeystoreHint.java
@@ -4,13 +4,17 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class DeviceKeystoreHint {
 
+    @JsonProperty("device_type")
+    public final String deviceType;
+
     @JsonProperty("device_id")
     public final String deviceId;
 
     @JsonProperty("hint")
     public final String hint;
 
-    public DeviceKeystoreHint(final String deviceId, final String hint) {
+    public DeviceKeystoreHint(final String deviceType, final String deviceId, final String hint) {
+        this.deviceType = deviceType;
         this.deviceId = deviceId;
         this.hint = hint;
     }
