fd729e94f279f5eab29d89338a1b143078e3352c
Tim Bart <tim@sayhello.com>
2015-01-25 15:06:59 -0800

    Clean up

--- suripu-core/src/main/java/com/hello/suripu/core/util/KeyStoreUtils.java
+++ suripu-core/src/main/java/com/hello/suripu/core/util/KeyStoreUtils.java
@@ -53,49 +53,35 @@ public class KeyStoreUtils {
         return toReturn;
     }
 
-    private static byte[] blockCipher(PrivateKey privateKey, byte[] bytes, int mode) throws IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
-        // string initialize 2 buffers.
-        // scrambled will hold intermediate results
+    private static byte[] blockCipher(final PrivateKey privateKey, byte[] bytes, int mode) throws IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+        // copied from http://coding.westreicher.org/?p=23
+        // TODO: rewrite or check license
+
         byte[] scrambled;
-
-        // toReturn will hold the total result
         byte[] toReturn = new byte[0];
-        // if we encrypt we use 100 byte long blocks. Decryption requires 128 byte long blocks (because of RSA)
-        int length = (mode == Cipher.ENCRYPT_MODE)? 100 : 128;
-
-        // another buffer. this one will hold the bytes that have to be modified in this step
-        byte[] buffer = new byte[length];
-
+        int length = 128;
+        byte[] buffer = new byte[128]; // RSA
 
         Cipher cipher = Cipher.getInstance("RSA/NONE/NoPadding");
         cipher.init(Cipher.DECRYPT_MODE, privateKey);
         for (int i=0; i< bytes.length; i++){
 
-            // if we filled our buffer array we have our block ready for de- or encryption
             if ((i > 0) && (i % length == 0)){
-                //execute the operation
+
                 scrambled = cipher.doFinal(buffer);
-                // add the result to our total result.
                 toReturn = append(toReturn,scrambled);
-                // here we calculate the length of the next buffer required
                 int newlength = length;
 
-                // if newlength would be longer than remaining bytes in the bytes array we shorten it.
                 if (i + length > bytes.length) {
                     newlength = bytes.length - i;
                 }
-                // clean the buffer array
+
                 buffer = new byte[newlength];
             }
-            // copy byte into our buffer.
             buffer[i%length] = bytes[i];
         }
 
-        // this step is needed if we had a trailing buffer. should only happen when encrypting.
-        // example: we encrypt 110 bytes. 100 bytes per run means we "forgot" the last 10 bytes. they are in the buffer array
         scrambled = cipher.doFinal(buffer);
-
-        // final step before we can return the modified data.
         toReturn = append(toReturn,scrambled);
 
         return toReturn;
