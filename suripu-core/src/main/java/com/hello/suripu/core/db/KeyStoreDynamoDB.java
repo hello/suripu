ab989ae0f0ae26aee04315df72128c2bfb5f04e4
Tim Bart <tim@sayhello.com>
2015-01-25 18:37:36 -0800

    duh

--- suripu-core/src/main/java/com/hello/suripu/core/db/KeyStoreDynamoDB.java
+++ suripu-core/src/main/java/com/hello/suripu/core/db/KeyStoreDynamoDB.java
@@ -36,7 +36,7 @@ public class KeyStoreDynamoDB implements KeyStore {
 
     private final static String DEVICE_ID_ATTRIBUTE_NAME = "device_id";
     private final static String AES_KEY_ATTRIBUTE_NAME = "aes_key";
-    private final static String DEFAULT_FACTORY_DEVICE_ID = "000000000";
+    private final static String DEFAULT_FACTORY_DEVICE_ID = "0000000000000000";
 
     private final byte[] DEFAULT_AES_KEY;
 
