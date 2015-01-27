2848f8f1f11848ef0d5fe2d32cfac60e48bad436
Pang Wu <pang@sayhello.com>
2015-01-27 15:29:13 -0800

    Rename files

--- suripu-workers/src/main/java/com/hello/suripu/workers/timeline/TimelineRecordProcessor.java
+++ suripu-workers/src/main/java/com/hello/suripu/workers/timeline/TimelineRecordProcessor.java
@@ -32,21 +32,21 @@ import java.util.Set;
 /**
  * Created by pangwu on 1/26/15.
  */
-public class TimeLineRecordProcessor extends HelloBaseRecordProcessor {
-    private final static Logger LOGGER = LoggerFactory.getLogger(TimeLineRecordProcessor.class);
+public class TimelineRecordProcessor extends HelloBaseRecordProcessor {
+    private final static Logger LOGGER = LoggerFactory.getLogger(TimelineRecordProcessor.class);
     private final TimelineProcessor timelineProcessor;
-    private final TimeLineWorkerConfiguration configuration;
+    private final TimelineWorkerConfiguration configuration;
     private final MergedUserInfoDynamoDB mergedUserInfoDynamoDB;
     private final RingTimeDAODynamoDB ringTimeDAODynamoDB;
     private final TimelineDAODynamoDB timelineDAODynamoDB;
     private final DeviceDAO deviceDAO;
 
-    public TimeLineRecordProcessor(final TimelineProcessor timelineProcessor,
+    public TimelineRecordProcessor(final TimelineProcessor timelineProcessor,
                                    final DeviceDAO deviceDAO,
                                    final MergedUserInfoDynamoDB mergedUserInfoDynamoDB,
                                    final RingTimeDAODynamoDB ringTimeDAODynamoDB,
                                    final TimelineDAODynamoDB timelineDAODynamoDB,
-                                   final TimeLineWorkerConfiguration configuration){
+                                   final TimelineWorkerConfiguration configuration){
 
         this.timelineProcessor = timelineProcessor;
         this.configuration = configuration;
