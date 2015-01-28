76edc2c5686818c91621a30638ee29c5f730a3b5
Pang Wu <pang@sayhello.com>
2015-01-28 15:55:14 -0800

    Let's dont rollout stuff that's not proofed

--- suripu-core/src/main/java/com/hello/suripu/core/util/TimelineUtils.java
+++ suripu-core/src/main/java/com/hello/suripu/core/util/TimelineUtils.java
@@ -893,25 +893,13 @@ public class TimelineUtils {
         // Heuristic fix
         if(sleep.getStartTimestamp() < goToBed.getStartTimestamp()) {
 
+            LOGGER.warn("Go to bed {} later then fall asleep {}, sleep set to go to bed.",
+                    new DateTime(goToBed.getStartTimestamp(), DateTimeZone.forOffsetMillis(goToBed.getTimezoneOffset())),
+                    new DateTime(sleep.getStartTimestamp(), DateTimeZone.forOffsetMillis(sleep.getTimezoneOffset())));
 
-            if (hasLongQuietPeriod(sleep.getStartTimestamp(), goToBed.getStartTimestamp(),
-                    features.get(MotionFeatures.FeatureType.MAX_NO_MOTION_PERIOD), 60)) {
-                LOGGER.warn("Go to bed {} later the fall asleep {}, sleep set to go to bed.",
-                        new DateTime(goToBed.getStartTimestamp(), DateTimeZone.forOffsetMillis(goToBed.getTimezoneOffset())),
-                        new DateTime(sleep.getStartTimestamp(), DateTimeZone.forOffsetMillis(sleep.getTimezoneOffset())));
-
-                fixedSleepEvents.set(1, new SleepEvent(goToBed.getStartTimestamp() + DateTimeConstants.MILLIS_PER_MINUTE,
-                        goToBed.getEndTimestamp() + DateTimeConstants.MILLIS_PER_MINUTE,
-                        goToBed.getTimezoneOffset()));
-            }else{
-                LOGGER.warn("Go to bed {} later the fall asleep {}, go to bed set to sleep.",
-                        new DateTime(goToBed.getStartTimestamp(), DateTimeZone.forOffsetMillis(goToBed.getTimezoneOffset())),
-                        new DateTime(sleep.getStartTimestamp(), DateTimeZone.forOffsetMillis(sleep.getTimezoneOffset())));
-
-                fixedSleepEvents.set(0, new InBedEvent(sleep.getStartTimestamp() - DateTimeConstants.MILLIS_PER_MINUTE,
-                        sleep.getEndTimestamp() - DateTimeConstants.MILLIS_PER_MINUTE,
-                        sleep.getTimezoneOffset()));
-            }
+            fixedSleepEvents.set(1, new SleepEvent(goToBed.getStartTimestamp() + DateTimeConstants.MILLIS_PER_MINUTE,
+                    goToBed.getEndTimestamp() + DateTimeConstants.MILLIS_PER_MINUTE,
+                    goToBed.getTimezoneOffset()));
 
         }
 
