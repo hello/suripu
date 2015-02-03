3502367860e498e5ef1cea216b11dd52cd67789d
Pang Wu <pang@sayhello.com>
2015-02-02 17:35:02 -0800

    Fix the test

--- suripu-algorithm/src/test/java/MotionScoreAlgorithmTest.java
+++ suripu-algorithm/src/test/java/MotionScoreAlgorithmTest.java
@@ -1,20 +1,10 @@
 import com.google.common.base.Optional;
-import com.hello.suripu.algorithm.core.AmplitudeData;
-import com.hello.suripu.algorithm.core.AmplitudeDataPreprocessor;
-import com.hello.suripu.algorithm.core.Segment;
 import com.hello.suripu.algorithm.pdf.LinearRankAscendingScoringFunction;
 import com.hello.suripu.algorithm.pdf.LinearRankDescendingScoringFunction;
 import com.hello.suripu.algorithm.pdf.RankPowerScoringFunction;
 import com.hello.suripu.algorithm.sleep.InternalScore;
 import com.hello.suripu.algorithm.sleep.MotionScoreAlgorithm;
-import com.hello.suripu.algorithm.sleep.scores.AmplitudeDataScoringFunction;
-import com.hello.suripu.algorithm.sleep.scores.LightOutScoringFunction;
-import com.hello.suripu.algorithm.sleep.scores.SleepDataScoringFunction;
-import com.hello.suripu.algorithm.utils.MaxAmplitudeAggregator;
-import com.hello.suripu.algorithm.utils.MotionFeatures;
 import org.joda.time.DateTime;
-import org.joda.time.DateTimeConstants;
-import org.joda.time.DateTimeZone;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -97,56 +87,4 @@ public class MotionScoreAlgorithmTest {
         assertThat(rankPowerScoringFunction.getScore(0d, pdf), is(0d));
     }
 
-
-    /*
-    * This test is to make sure I made no stupid mistakes in porting algorithm from python to java
-     */
-    @Test
-    public void testJavaCodeWorksTheSameAsPythonPrototype(){
-        // first light out: 1417598760000
-        final DateTime firstLightOutTime = new DateTime(1417598760000L, DateTimeZone.UTC);
-        final MotionFixtureCSVDataSource dataSource = new MotionFixtureCSVDataSource("pang_motion_2014_12_02_gap_filled.csv");
-        // Raw data count 791
-        assertThat(dataSource.getDataForDate(new DateTime(2014, 12, 02, 0, 0, DateTimeZone.UTC)).size(), is(791));
-
-        final AmplitudeDataPreprocessor smoother = new MaxAmplitudeAggregator(MotionFeatures.MOTION_AGGREGATE_WINDOW_IN_MINUTES * DateTimeConstants.MILLIS_PER_MINUTE);
-        final List<AmplitudeData> smoothedData = smoother.process(dataSource.getDataForDate(new DateTime(2014, 12, 02, 0, 0, DateTimeZone.UTC)));
-
-        final ArrayList<SleepDataScoringFunction> scoringFunctions = new ArrayList<>();
-        scoringFunctions.add(new AmplitudeDataScoringFunction());
-        scoringFunctions.add(new LightOutScoringFunction(firstLightOutTime, 3d));
-
-        final Map<Long, List<AmplitudeData>> matrix = MotionScoreAlgorithm.createFeatureMatrix(smoothedData);
-        for(final Long timestamp:matrix.keySet()){
-            final List<AmplitudeData> dataVector = matrix.get(timestamp);
-            if(timestamp < firstLightOutTime.getMillis()){
-                dataVector.add(new AmplitudeData(timestamp, 0d, dataVector.get(0).offsetMillis));
-            }else{
-                dataVector.add(new AmplitudeData(timestamp, 1d, dataVector.get(0).offsetMillis));
-            }
-        }
-        final MotionScoreAlgorithm algorithm = new MotionScoreAlgorithm(matrix, 2, smoothedData.size(), scoringFunctions);
-        final List<Segment> sleepSegments = algorithm.getSleepEvents(true);
-        //final Segment sleepSegment = sleepSegments.get(1);
-        final Segment goToBedSegment = sleepSegments.get(0);
-        final Segment outOfBedSegment = sleepSegments.get(3);
-
-
-        // Out put from python script suripu_light_test.py:
-        /*
-        sleep at 2014-12-03 01:39:00, prob: 1.06747942852, amp: 5471
-        wake up at 2014-12-03 07:09:00, prob: 0.0924221378596, amp: 518
-        */
-
-        final DateTime goToBedTime = new DateTime(goToBedSegment.getStartTimestamp(), DateTimeZone.forOffsetMillis(goToBedSegment.getOffsetMillis()));
-        //final DateTime sleepTime = new DateTime(sleepSegment.getStartTimestamp(), DateTimeZone.forOffsetMillis(sleepSegment.getOffsetMillis()));
-        final DateTime outOfBedTime = new DateTime(outOfBedSegment.getStartTimestamp(), DateTimeZone.forOffsetMillis(outOfBedSegment.getOffsetMillis()));
-
-        final DateTime goToBedLocalUTC = new DateTime(goToBedTime.getYear(), goToBedTime.getMonthOfYear(), goToBedTime.getDayOfMonth(), goToBedTime.getHourOfDay(), goToBedTime.getMinuteOfHour(), DateTimeZone.UTC);
-        //final DateTime sleepLocalUTC = new DateTime(sleepTime.getYear(), sleepTime.getMonthOfYear(), sleepTime.getDayOfMonth(), sleepTime.getHourOfDay(), sleepTime.getMinuteOfHour(), DateTimeZone.UTC);
-        final DateTime outOfBedLocalUTC = new DateTime(outOfBedTime.getYear(), outOfBedTime.getMonthOfYear(), outOfBedTime.getDayOfMonth(), outOfBedTime.getHourOfDay(), outOfBedTime.getMinuteOfHour(), DateTimeZone.UTC);
-
-        assertThat(goToBedLocalUTC, is(new DateTime(2014, 12, 03, 1, 39, DateTimeZone.UTC)));
-        assertThat(outOfBedLocalUTC, is(new DateTime(2014, 12, 03, 7, 9, DateTimeZone.UTC)));
-    }
 }
