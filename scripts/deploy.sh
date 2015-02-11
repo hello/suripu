ccc030d604c70848ca8fef8c69f82f27aa8da5c5
Tim Bart <tim@sayhello.com>
2015-02-11 10:38:56 -0800

    Adding our sketchy deploy scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,88 @@
+if [ -z "$1" ]
+  then
+    echo "[ERROR] \xE2\x9A\xA0 Missing version number"
+    exit 1
+fi
+
+echo "Updating symlink for suripu-app"
+rm /home/build/build/suripu-app.jar 
+ln -s /home/build/build/suripu-app-$1.jar /home/build/build/suripu-app.jar
+
+echo "Copying configs to /etc/"
+cp /home/build/build/suripu-app.prod.yml /etc/
+
+echo "restarting..."
+restart suripuapp
+
+echo "Sleeping 30s to let suripu-app restart properly"
+sleep 30
+
+echo "Updating symlink for suripu-service"
+rm /home/build/build/suripu-service.jar
+ln -s /home/build/build/suripu-service-$1.jar /home/build/build/suripu-service.jar
+
+echo "Moving configs to /etc/"
+cp /home/build/build/suripu-service.prod.yml /etc/
+
+echo "restarting..."
+restart suripuservice
+
+sleep 1
+
+echo "Updating symlink for suripu-workers"
+rm /home/build/build/suripu-workers.jar
+ln -s /home/build/build/suripu-workers-$1.jar /home/build/build/suripu-workers.jar
+
+echo "Moving pillscorer configs to /etc/"
+cp /home/build/build/pillscorer.prod.yml /etc/
+
+echo "restarting..."
+restart suripuworkers-pillscorer
+
+sleep 1
+
+echo "Moving pilldata configs to /etc/"
+cp /home/build/build/pill.prod.yml /etc/
+
+echo "restarting..."
+restart suripuworkers-pilldata
+
+sleep 1
+
+echo "Moving smart alarm configs to /etc/"
+cp /home/build/build/alarm_worker.prod.yml /etc/
+
+echo "restarting..."
+restart suripuworkers-smartalarm
+
+sleep 1
+
+echo "Moving sense save configs to /etc/"
+cp /home/build/build/sense_save.prod.yml /etc/
+
+echo "restarting"
+restart suripuworkers-sense
+
+sleep 1
+
+echo "Moving index log worker configs to /etc/"
+cp /home/build/build/index_logs_worker.prod.yml /etc/
+
+echo "restarting"
+restart suripuworkers-index-logs
+
+sleep 1
+
+echo "Moving insights generator worker configs to /etc/"
+cp /home/build/build/insights_generator.prod.yml /etc/
+
+echo "restarting"
+restart suripuworkers-insights
+
+sleep 1
+
+echo "Moving insights generator worker configs to /etc/"
+cp /home/build/build/timeline_worker.prod.yml /etc/
+
+echo "restarting"
+restart suripuworkers-timeline
