205aeb3be51a6641667a2ac6401ded20c504302d
Pang Wu <pang@sayhello.com>
2015-03-02 18:19:22 -0800

    Upload to s3

--- research_build.sh
+++ research_build.sh
@@ -2,7 +2,7 @@
 
 git checkout research
 git pull
-mvn clean test verify package
+mvn clean test package
 
-scp suripu-research/target/suripu-research-*-SNAPSHOT.jar build@ip-10-0-0-20.ec2.internal:/build/suripu-research.jar
-scp suripu-research/suripu-research.prod.yml build@ip-10-0-0-20.ec2.internal:/build
+s3cmd put suripu-research/suripu-research.prod.yml s3://hello-research-deploy/suripu-research.prod.yml
+s3cmd put suripu-research/target/suripu-research-*-SNAPSHOT.jar s3://hello-research-deploy/suripu-research.jar
