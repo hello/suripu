1a361c0e347be9dbe78eef56b1076c803eeca67f
Josef Norgan <josef@sayhello.com>
2015-03-03 17:52:14 -0800

    Minor code style cleanup.

--- suripu-core/src/test/java/com/hello/suripu/core/processors/OTAProcessorTest.java
+++ suripu-core/src/test/java/com/hello/suripu/core/processors/OTAProcessorTest.java
@@ -1,7 +1,7 @@
 package com.hello.suripu.core.processors;
 
+import com.google.common.collect.Sets;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import org.joda.time.DateTime;
@@ -22,8 +22,7 @@ public class OTAProcessorTest {
 
         String deviceID = "fake-sense";
         final List<String> deviceGroups = Arrays.asList("group1", "group2", "group3");
-        final Set<String> overrideOTAGroups = new HashSet<String>();
-        overrideOTAGroups.add("override");
+        final Set<String> overrideOTAGroups =  Sets.newHashSet("override");
         final DateTimeZone userTimeZone = DateTimeZone.UTC;
         final Integer deviceUptimeDelayMinutes = 20;
         final Integer uptimeInSeconds = 10 * DateTimeConstants.SECONDS_PER_MINUTE; //This should invalidate all cases that aren't 'alwaysOTA'
@@ -41,8 +40,7 @@ public class OTAProcessorTest {
 
         String deviceID = "fake-sense";
         final List<String> deviceGroups = Arrays.asList("group1", "group2", "group3");
-        final Set<String> overrideOTAGroups = new HashSet<String>();
-        overrideOTAGroups.add("override");
+        final Set<String> overrideOTAGroups =  Sets.newHashSet("override");
         final DateTimeZone userTimeZone = DateTimeZone.UTC;
         final Integer deviceUptimeDelayMinutes = 20;
         final Integer uptimeInSeconds = 21 * DateTimeConstants.SECONDS_PER_MINUTE;
@@ -62,8 +60,7 @@ public class OTAProcessorTest {
 
         String deviceID = "fake-sense";
         final List<String> deviceGroups = Arrays.asList("group1", "group2", "group3");
-        final Set<String> overrideOTAGroups = new HashSet<String>();
-        overrideOTAGroups.add("override");
+        final Set<String> overrideOTAGroups =  Sets.newHashSet("override");
         final DateTimeZone userTimeZone = DateTimeZone.UTC;
         final DateTime goodDTZ = new DateTime(userTimeZone).withHourOfDay(14).withMinuteOfHour(0);
         final DateTime startOTAWindow = new DateTime(userTimeZone).withHourOfDay(11).withMinuteOfHour(0);
@@ -80,10 +77,8 @@ public class OTAProcessorTest {
 
         String deviceID = "fake-sense";
         final List<String> deviceGroups = Arrays.asList("group1", "group2", "group3");
-        final Set<String> overrideOTAGroups = new HashSet<String>();
-        overrideOTAGroups.add("group1");
-        final Set<String> nonOverrideOTAGroups = new HashSet<String>();
-        overrideOTAGroups.add("group4");
+        final Set<String> overrideOTAGroups =  Sets.newHashSet("group1");
+        final Set<String> nonOverrideOTAGroups =  Sets.newHashSet("group4");
         final DateTimeZone userTimeZone = DateTimeZone.UTC;
         final Integer deviceUptimeDelayMinutes = 20;
         final Integer uptimeInSeconds = 19 * DateTimeConstants.SECONDS_PER_MINUTE; //Should cause canOTA to be false unless overridden by group membership
