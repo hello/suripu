818345cd95de524ead2ab443c40ce3ac5fe41d7f
longd3 <izzy3ds@gmail.com>
2015-03-09 13:04:00 -0700

    Initial commits to spin out admin

--- suripu-admin/src/main/java/com/hello/suripu/admin/resources/v1/AccountResource.java
+++ suripu-admin/src/main/java/com/hello/suripu/admin/resources/v1/AccountResource.java
@@ -1,20 +1,49 @@
 package com.hello.suripu.admin.resources.v1;
 
+import com.google.common.base.Optional;
+import com.hello.suripu.core.db.AccountDAO;
 import com.hello.suripu.core.models.Account;
+import com.hello.suripu.core.oauth.AccessToken;
+import com.hello.suripu.core.oauth.OAuthScope;
+import com.hello.suripu.core.oauth.Scope;
+import com.yammer.metrics.annotation.Timed;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-@Path("/admin/v1/account")
+@Path("/v1/account")
 public class AccountResource {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AccountResource.class);
+    private final AccountDAO accountDAO;
+
+    public AccountResource(final AccountDAO accountDAO) {
+        this.accountDAO = accountDAO;
+    }
+
     @GET
+    @Timed
     @Produces(MediaType.APPLICATION_JSON)
-    public Account getAccount() {
+    public Account getAccountByEmail(@Scope({OAuthScope.ADMINISTRATION_READ}) final AccessToken accessToken,
+                                     @QueryParam("email") final String email) {
+        LOGGER.debug("Looking up {}", email);
 
-        return null;
+        final Optional<Account> accountOptional = accountDAO.getByEmail(email);
 
+        if (!accountOptional.isPresent()) {
+            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
+                    .entity("Account not found").build());
+        }
+
+        else {
+            return accountOptional.get();
+        }
     }
 }
