4cb31de4398667babc5621883597f27eaf1223c5
Josef Norgan <josef@sayhello.com>
2015-03-11 16:08:44 -0700

    Returning score along with devices in seen fw device list
    Added new endpoint for getting all seen firmwares

--- suripu-app/src/main/java/com/hello/suripu/app/resources/v1/FirmwareResource.java
+++ suripu-app/src/main/java/com/hello/suripu/app/resources/v1/FirmwareResource.java
@@ -2,11 +2,13 @@ package com.hello.suripu.app.resources.v1;
 
 import com.hello.suripu.core.db.DeviceDAO;
 import com.hello.suripu.core.models.FirmwareInfo;
+import com.hello.suripu.core.models.FirmwareCountInfo;
 import com.hello.suripu.core.oauth.AccessToken;
 import com.hello.suripu.core.oauth.OAuthScope;
 import com.hello.suripu.core.oauth.Scope;
 import com.yammer.metrics.annotation.Timed;
-import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 import javax.ws.rs.QueryParam;
 import org.slf4j.Logger;
@@ -21,6 +23,7 @@ import javax.ws.rs.core.Response;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Tuple;
 
 @Path("/v1/firmware")
 public class FirmwareResource {
@@ -39,7 +42,7 @@ public class FirmwareResource {
     @Timed
     @Path("/devices")
     @Produces(MediaType.APPLICATION_JSON)
-    public FirmwareInfo getFirmwareDeviceList(@Scope(OAuthScope.ADMINISTRATION_READ) final AccessToken accessToken,
+    public LinkedList<FirmwareInfo> getFirmwareDeviceList(@Scope(OAuthScope.ADMINISTRATION_READ) final AccessToken accessToken,
                                               @QueryParam("firmware_version") final Long firmwareVersion) {
         if(firmwareVersion == null) {
             LOGGER.error("Missing firmwareVersion parameter");
@@ -48,17 +51,20 @@ public class FirmwareResource {
 
         final Jedis jedis = jedisPool.getResource();
         final String fwVersion = firmwareVersion.toString();
-        Set<String> allFWDevices = new HashSet<>();
-
+        LinkedList<FirmwareInfo> deviceInfo = new LinkedList<>();
         try {
-            allFWDevices = jedis.zrevrange(fwVersion, 0, -1);
+
+            final Set<Tuple> allFWDevices = jedis.zrevrangeWithScores(fwVersion, 0, -1);
+            for(Tuple device: allFWDevices){
+                deviceInfo.add(new FirmwareInfo(fwVersion, device.getElement(), (long)device.getScore()));
+            }
         } catch (Exception e) {
             LOGGER.error("Failed retrieving firmware device count.", e.getMessage());
         } finally {
             jedisPool.returnResource(jedis);
         }
 
-        return new FirmwareInfo(fwVersion, allFWDevices);
+        return deviceInfo;
     }
 
     @GET
@@ -88,5 +94,26 @@ public class FirmwareResource {
         return devicesOnFirmware;
     }
 
+    @GET
+    @Timed
+    @Path("/list")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<FirmwareCountInfo> getAllSeenFirmwares(@Scope(OAuthScope.ADMINISTRATION_READ) final AccessToken accessToken) {
 
+        final Jedis jedis = jedisPool.getResource();
+        List<FirmwareCountInfo> firmwareCounts = new LinkedList<>();
+        try {
+            final Set<String> seenFirmwares = jedis.smembers("firmwares_seen");
+            for (String fw_version:seenFirmwares) {
+                final long fwCount = jedis.zcard(fw_version);
+                firmwareCounts.add(new FirmwareCountInfo(fw_version, fwCount));
+            }
+        } catch (Exception e) {
+            LOGGER.error("Failed retrieving all seen firmwares.", e.getMessage());
+        } finally {
+            jedisPool.returnResource(jedis);
+        }
+
+        return firmwareCounts;
+    }
 }
