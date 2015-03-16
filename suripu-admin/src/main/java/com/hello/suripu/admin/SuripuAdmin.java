8970e4030fd3b601c414367c5acb6eb994adeccd
longd3 <izzy3ds@gmail.com>
2015-03-16 12:10:18 -0700

    Add admin device resources with custom returns
    - Sense endpoint returning sense info & sense status
    - Pill endpoint returning pill info & pill status

--- suripu-admin/src/main/java/com/hello/suripu/admin/SuripuAdmin.java
+++ suripu-admin/src/main/java/com/hello/suripu/admin/SuripuAdmin.java
@@ -1,12 +1,23 @@
 package com.hello.suripu.admin;
 
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.hello.dropwizard.mikkusu.resources.PingResource;
 import com.hello.suripu.admin.configuration.SuripuAdminConfiguration;
-import com.hello.suripu.admin.resources.v1.AccountResource;
+import com.hello.suripu.admin.resources.v1.AccountResources;
+import com.hello.suripu.admin.resources.v1.DeviceResources;
 import com.hello.suripu.core.bundles.KinesisLoggerBundle;
+import com.hello.suripu.core.clients.AmazonDynamoDBClientFactory;
 import com.hello.suripu.core.configuration.KinesisLoggerConfiguration;
 import com.hello.suripu.core.db.AccountDAO;
 import com.hello.suripu.core.db.AccountDAOImpl;
+import com.hello.suripu.core.db.DeviceDAO;
+import com.hello.suripu.core.db.DeviceDataDAO;
+import com.hello.suripu.core.db.KeyStore;
+import com.hello.suripu.core.db.KeyStoreDynamoDB;
+import com.hello.suripu.core.db.MergedUserInfoDynamoDB;
+import com.hello.suripu.core.db.TrackerMotionDAO;
 import com.yammer.dropwizard.Service;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
@@ -41,9 +52,43 @@ public class SuripuAdmin extends Service<SuripuAdminConfiguration> {
     public void run(SuripuAdminConfiguration configuration, Environment environment) throws Exception {
         final DBIFactory factory = new DBIFactory();
         final DBI commonDB = factory.build(environment, configuration.getCommonDB(), "postgresql");
+        final DBI sensorsDB = factory.build(environment, configuration.getSensorsDB(), "postgresql");
+
+        final AWSCredentialsProvider awsCredentialsProvider= new DefaultAWSCredentialsProviderChain();
+        final AmazonDynamoDBClientFactory dynamoDBClientFactory = AmazonDynamoDBClientFactory.create(awsCredentialsProvider);
+
+
+
         final AccountDAO accountDAO = commonDB.onDemand(AccountDAOImpl.class);
+        final DeviceDAO deviceDAO = commonDB.onDemand(DeviceDAO.class);
+        final DeviceDataDAO deviceDataDAO = sensorsDB.onDemand(DeviceDataDAO.class);
+        final TrackerMotionDAO trackerMotionDAO = sensorsDB.onDemand(TrackerMotionDAO.class);
+
+
+
+        final AmazonDynamoDB mergedUserInfoDynamoDBClient = dynamoDBClientFactory.getForEndpoint(configuration.getUserInfoDynamoDBConfiguration().getEndpoint());
+        final MergedUserInfoDynamoDB mergedUserInfoDynamoDB = new MergedUserInfoDynamoDB(
+                mergedUserInfoDynamoDBClient, configuration.getUserInfoDynamoDBConfiguration().getTableName()
+        );
+
+        final AmazonDynamoDB senseKeyStoreDynamoDBClient = dynamoDBClientFactory.getForEndpoint(configuration.getSenseKeyStoreDynamoDBConfiguration().getEndpoint());
+        final KeyStore senseKeyStore = new KeyStoreDynamoDB(
+                senseKeyStoreDynamoDBClient,
+                configuration.getSenseKeyStoreDynamoDBConfiguration().getTableName(),
+                "1234567891234567".getBytes(), // TODO: REMOVE THIS WHEN WE ARE NOT SUPPOSED TO HAVE A DEFAULT KEY
+                120 // 2 minutes for cache
+        );
+
+        final AmazonDynamoDB pillKeyStoreDynamoDBClient = dynamoDBClientFactory.getForEndpoint(configuration.getPillKeyStoreDynamoDBConfiguration().getEndpoint());
+        final KeyStore pillKeyStore = new KeyStoreDynamoDB(
+                pillKeyStoreDynamoDBClient,
+                configuration.getPillKeyStoreDynamoDBConfiguration().getTableName(),
+                "9876543219876543".getBytes(), // TODO: REMOVE THIS WHEN WE ARE NOT SUPPOSED TO HAVE A DEFAULT KEY
+                120 // 2 minutes for cache
+        );
 
         environment.addResource(new PingResource());
-        environment.addResource(new AccountResource(accountDAO));
+        environment.addResource(new AccountResources(accountDAO));
+        environment.addResource(new DeviceResources(deviceDAO, deviceDataDAO, trackerMotionDAO, accountDAO, mergedUserInfoDynamoDB, senseKeyStore, pillKeyStore));
     }
 }
