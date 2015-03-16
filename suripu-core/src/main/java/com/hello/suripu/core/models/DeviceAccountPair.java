00f2d20cf4ce013f9ef6b00267e2552e70f925ec
longd3 <izzy3ds@gmail.com>
2015-03-16 15:56:18 -0700

    Ignore DeviceAccountPair created when returning json

--- suripu-core/src/main/java/com/hello/suripu/core/models/DeviceAccountPair.java
+++ suripu-core/src/main/java/com/hello/suripu/core/models/DeviceAccountPair.java
@@ -1,5 +1,6 @@
 package com.hello.suripu.core.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.joda.time.DateTime;
 
 public class DeviceAccountPair {
@@ -7,6 +8,8 @@ public class DeviceAccountPair {
     public final Long accountId;
     public final Long internalDeviceId;
     public final String externalDeviceId;
+
+    @JsonIgnore
     public final DateTime created;
 
     public DeviceAccountPair(final Long accountId, final Long internalDeviceId, final String externalDeviceId, final DateTime created) {
