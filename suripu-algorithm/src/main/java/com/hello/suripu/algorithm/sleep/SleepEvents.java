86639d1e7f0128863e6fb7ca9f910edbc509f55c
Benjamin Joseph <benjo@sayhello.com>
2015-03-17 09:02:27 -0700

    undo changes to sleepevents.java since I'm running a separate timeline function now

--- suripu-algorithm/src/main/java/com/hello/suripu/algorithm/sleep/SleepEvents.java
+++ suripu-algorithm/src/main/java/com/hello/suripu/algorithm/sleep/SleepEvents.java
@@ -1,9 +1,6 @@
 package com.hello.suripu.algorithm.sleep;
 
-import com.google.common.collect.ImmutableList;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -14,22 +11,16 @@ public class SleepEvents<T> {
     public final T fallAsleep;
     public final T wakeUp;
     public final T outOfBed;
-    public final List<T> disturbances;
 
-    private SleepEvents(final T goToBed, final T fallAsleep, final T wakeUp, final T outOfBed,List<T> disturbances){
+    private SleepEvents(final T goToBed, final T fallAsleep, final T wakeUp, final T outOfBed){
         this.goToBed = goToBed;
         this.fallAsleep = fallAsleep;
         this.wakeUp = wakeUp;
         this.outOfBed = outOfBed;
-        this.disturbances = disturbances;
     }
 
     public static <T> SleepEvents<T> create(final T goToBed, final T fallAsleep, final T wakeUp, final T outOfBed){
-        return new SleepEvents(goToBed, fallAsleep, wakeUp, outOfBed, Collections.EMPTY_LIST);
-    }
-
-    public static <T> SleepEvents<T> create(final T goToBed, final T fallAsleep, final T wakeUp, final T outOfBed,List<T> disturbances){
-        return new SleepEvents(goToBed, fallAsleep, wakeUp, outOfBed,disturbances);
+        return new SleepEvents(goToBed, fallAsleep, wakeUp, outOfBed);
     }
 
     public List<T> toList(){
@@ -39,8 +30,6 @@ public class SleepEvents<T> {
         list.add(this.wakeUp);
         list.add(this.outOfBed);
 
-        list.addAll(this.disturbances);
-
-        return (list);
+        return list;
     }
 }
