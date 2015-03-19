78efb6b87879e2efce0d886eadfb36a57b6b0a39
Tim Bart <tim@sayhello.com>
2015-03-19 14:46:04 -0700

    Don't list the example worker in available commands

--- suripu-workers/src/main/java/com/hello/suripu/workers/HelloWorker.java
+++ suripu-workers/src/main/java/com/hello/suripu/workers/HelloWorker.java
@@ -3,7 +3,6 @@ package com.hello.suripu.workers;
 import com.hello.suripu.core.bundles.KinesisLoggerBundle;
 import com.hello.suripu.core.configuration.KinesisLoggerConfiguration;
 import com.hello.suripu.workers.alarm.AlarmWorkerCommand;
-import com.hello.suripu.workers.framework.ExampleWorker;
 import com.hello.suripu.workers.framework.WorkerConfiguration;
 import com.hello.suripu.workers.insights.InsightsGeneratorWorkerCommand;
 import com.hello.suripu.workers.logs.LogIndexerWorkerCommand;
@@ -41,7 +40,6 @@ public class HelloWorker extends Service<WorkerConfiguration> {
         bootstrap.addCommand(new InsightsGeneratorWorkerCommand("insights_generator", "generate insights for users"));
         bootstrap.addCommand(new TimelineWorkerCommand("timeline", "generate timeline for users"));
         bootstrap.addCommand(new PushNotificationsWorkerCommand("push", "send push notifications"));
-        bootstrap.addCommand(new ExampleWorker("example", "example stuff"));
     }
 
     @Override
