255b1b0ba7c4b526eb1441908bebc9a77e7cf7b1
longd3 <izzy3ds@gmail.com>
2015-03-27 19:01:28 -0700

    Add an admin api to set password on behalf of a customer

--- suripu-admin/src/main/java/com/hello/suripu/admin/models/PasswordResetAdmin.java
+++ suripu-admin/src/main/java/com/hello/suripu/admin/models/PasswordResetAdmin.java
@@ -1,11 +1,8 @@
-package com.hello.suripu.core.models;
+package com.hello.suripu.admin.models;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-/**
- * Created by zet on 3/27/15.
- */
 public class PasswordResetAdmin {
     public final String email;
     public final String password;
