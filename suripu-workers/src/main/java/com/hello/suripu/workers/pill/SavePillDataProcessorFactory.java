0287ad5fff3fb64a0188d50daf3baa6556703368
Tim Bart <tim@sayhello.com>
2015-04-09 16:26:05 -0700

    Tracking active pills from different pill worker

--- suripu-workers/src/main/java/com/hello/suripu/workers/pill/SavePillDataProcessorFactory.java
+++ suripu-workers/src/main/java/com/hello/suripu/workers/pill/SavePillDataProcessorFactory.java
@@ -7,6 +7,7 @@ import com.hello.suripu.core.db.KeyStore;
 import com.hello.suripu.core.db.MergedUserInfoDynamoDB;
 import com.hello.suripu.core.db.PillHeartBeatDAO;
 import com.hello.suripu.core.db.TrackerMotionDAO;
+import com.hello.suripu.workers.utils.ActiveDevicesTracker;
 
 public class SavePillDataProcessorFactory implements IRecordProcessorFactory {
 
@@ -16,6 +17,7 @@ public class SavePillDataProcessorFactory implements IRecordProcessorFactory {
     private final KeyStore pillKeyStore;
     private final PillHeartBeatDAO pillHeartBeatDAO;
     private final DeviceDAO deviceDAO;
+    private final ActiveDevicesTracker activeDevicesTracker;
 
     public SavePillDataProcessorFactory(
             final TrackerMotionDAO trackerMotionDAO,
@@ -23,17 +25,19 @@ public class SavePillDataProcessorFactory implements IRecordProcessorFactory {
             final MergedUserInfoDynamoDB mergedUserInfoDynamoDB,
             final PillHeartBeatDAO pillHeartBeatDAO,
             final KeyStore pillKeyStore,
-            final DeviceDAO deviceDAO) {
+            final DeviceDAO deviceDAO,
+            final ActiveDevicesTracker activeDevicesTracker) {
         this.trackerMotionDAO = trackerMotionDAO;
         this.batchSize = batchSize;
         this.mergedUserInfoDynamoDB= mergedUserInfoDynamoDB;
         this.pillHeartBeatDAO = pillHeartBeatDAO;
         this.pillKeyStore = pillKeyStore;
         this.deviceDAO = deviceDAO;
+        this.activeDevicesTracker = activeDevicesTracker;
     }
 
     @Override
     public IRecordProcessor createProcessor() {
-        return new SavePillDataProcessor(trackerMotionDAO, batchSize, pillHeartBeatDAO, pillKeyStore, deviceDAO, mergedUserInfoDynamoDB);
+        return new SavePillDataProcessor(trackerMotionDAO, batchSize, pillHeartBeatDAO, pillKeyStore, deviceDAO, mergedUserInfoDynamoDB, activeDevicesTracker);
     }
 }
