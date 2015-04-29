eb1c38dbd8e2f3c4663034187d9418a655342191
longd3 <izzy3ds@gmail.com>
2015-04-29 16:33:11 -0700

    Add endpoint to retrieve partner account

--- suripu-admin/src/main/java/com/hello/suripu/admin/SuripuAdmin.java
+++ suripu-admin/src/main/java/com/hello/suripu/admin/SuripuAdmin.java
@@ -189,7 +189,7 @@ public class SuripuAdmin extends Service<SuripuAdminConfiguration> {
         final ResponseCommandsDAODynamoDB respCommandsDAODynamoDB = new ResponseCommandsDAODynamoDB(respCommandsDynamoDBClient, configuration.getResponseCommandsDBConfiguration().getTableName());
 
         environment.addResource(new PingResource());
-        environment.addResource(new AccountResources(accountDAO, passwordResetDB));
+        environment.addResource(new AccountResources(accountDAO, passwordResetDB, deviceDAO));
         environment.addResource(new DeviceResources(deviceDAO, deviceDAOAdmin, deviceDataDAO, trackerMotionDAO, accountDAO, mergedUserInfoDynamoDB, senseKeyStore, pillKeyStore, jedisPool, pillHeartBeatDAO));
         environment.addResource(new DataResources(deviceDataDAO, deviceDAO, accountDAO, userLabelDAO, trackerMotionDAO));
         environment.addResource(new ApplicationResources(applicationStore));
