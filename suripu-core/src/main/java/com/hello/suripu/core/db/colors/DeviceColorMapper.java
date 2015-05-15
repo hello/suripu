9e0b85e8d294ae884f0612fe1f98348cc91a9825
Tim Bart <tim@sayhello.com>
2015-05-14 17:02:59 -0700

    Missing classes

---
+++ suripu-core/src/main/java/com/hello/suripu/core/db/colors/DeviceColorMapper.java
@@ -0,0 +1,15 @@
+package com.hello.suripu.core.db.colors;
+
+import com.hello.suripu.core.models.Device;
+import org.skife.jdbi.v2.StatementContext;
+import org.skife.jdbi.v2.tweak.ResultSetMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DeviceColorMapper implements ResultSetMapper<Device.Color> {
+    @Override
+    public Device.Color map(int index, ResultSet r, StatementContext ctx) throws SQLException {
+        return Device.Color.valueOf(r.getString("color"));
+    }
+}
---
+++ suripu-core/src/main/java/com/hello/suripu/core/db/colors/SenseColorDAOSQLImpl.java
@@ -0,0 +1,24 @@
+package com.hello.suripu.core.db.colors;
+
+import com.google.common.base.Optional;
+import com.hello.suripu.core.models.Device;
+import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
+import org.skife.jdbi.v2.sqlobject.customizers.SingleValueResult;
+
+public abstract class SenseColorDAOSQLImpl implements SenseColorDAO {
+
+    @SingleValueResult
+    @RegisterMapper(DeviceColorMapper.class)
+    @SqlQuery("SELECT * FROM sense_colors WHERE sense_id = :sense_id LIMIT 1")
+    public abstract Optional<Device.Color> getColorForSense(@Bind("sense_id") final String senseId);
+
+
+    @SqlUpdate("INSERT INTO sense_colors (sense_id, color) VALUES(:sense_id, :color);")
+    public abstract int saveColorForSense(@Bind("sense_id") final String senseId, @Bind("color") final String color);
+
+    @SqlUpdate("UPDATE sense_colors SET color = :color WHERE sense_id = :sense_id;")
+    public abstract int update(@Bind("sense_id") final String senseId, @Bind("color") final String color);
+}
