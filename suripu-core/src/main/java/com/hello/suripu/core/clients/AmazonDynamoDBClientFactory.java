8985d220e6efccd62472b6537e264a145fd9360b
Tim Bart <tim@sayhello.com>
2015-06-15 10:46:55 -0700

    First pass at instrumented DynamoDB client

--- suripu-core/src/main/java/com/hello/suripu/core/clients/AmazonDynamoDBClientFactory.java
+++ suripu-core/src/main/java/com/hello/suripu/core/clients/AmazonDynamoDBClientFactory.java
@@ -7,6 +7,7 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.google.common.collect.Maps;
 import com.hello.suripu.core.configuration.DynamoDBTableName;
 import com.hello.suripu.core.configuration.NewDynamoDBConfiguration;
+import com.hello.suripu.core.metrics.InstrumentedDynamoDBClient;
 
 import java.util.Map;
 
@@ -15,6 +16,7 @@ public class AmazonDynamoDBClientFactory {
     private final AWSCredentialsProvider awsCredentialsProvider;
     private final ClientConfiguration clientConfiguration;
     private final Map<String, AmazonDynamoDB> clients = Maps.newHashMap();
+    private final Map<String, AmazonDynamoDB> instrumentedClients = Maps.newHashMap(); // key is className
     private final NewDynamoDBConfiguration dynamoDBConfiguration;
 
     private final static ClientConfiguration DEFAULT_CLIENT_CONFIGURATION = new ClientConfiguration().withConnectionTimeout(200).withMaxErrorRetry(1);
@@ -67,6 +69,22 @@ public class AmazonDynamoDBClientFactory {
         final AmazonDynamoDB client = new AmazonDynamoDBClient(awsCredentialsProvider, clientConfiguration);
         client.setEndpoint(endpoint);
         clients.put(endpoint, client);
+        return client;
+    }
+
+    public synchronized AmazonDynamoDB getInstrumented(final DynamoDBTableName tableName, final Class<?> klass) {
+        if(!dynamoDBConfiguration.tables().containsKey(tableName) || !dynamoDBConfiguration.endpoints().containsKey(tableName)) {
+            throw new IllegalArgumentException("Check configuration. Invalid tableName: " + tableName.toString());
+        }
+
+        final String endpoint = dynamoDBConfiguration.endpoints().get(tableName);
+        if(instrumentedClients.containsKey(klass.getName())) {
+            return instrumentedClients.get(klass.getName());
+        }
+
+        final AmazonDynamoDB client = new InstrumentedDynamoDBClient(new AmazonDynamoDBClient(awsCredentialsProvider, clientConfiguration), klass);
+        client.setEndpoint(endpoint);
+        instrumentedClients.put(klass.getName(), client);
         return client;
     }
 }
