d6521fa09c5d0b77f6b65f21a1d824257368a41d
longd3 <izzy3ds@gmail.com>
2015-06-24 12:57:09 -0700

    Update es document fields to include helpful boolean attributes

--- suripu-core/src/main/java/com/hello/suripu/core/models/SenseLogDocument.java
+++ suripu-core/src/main/java/com/hello/suripu/core/models/SenseLogDocument.java
@@ -7,15 +7,29 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import java.util.Map;
 
 public class SenseLogDocument {
+    private static final String ALARM_RINGING_REGEX = "(?s)^.*?(ALARM RINGING).*$";
+    private static final String FIRMWARE_CRASH_REGEX = "(?s)^.*?(xkd|travis|fault).*$";
+    private static final String WIFI_INFO_REGEX = "(?s)^.*?(SSID RSSI UNIQUE).*$";
+    private static final String DUST_REGEX = "(?s)^.*?(dust).*$";
+
     public final String senseId;
     public final Long timestamp;
     public final String content;
     public final String origin;
+    public final Boolean hasAlarm;
+    public final Boolean hasFirmwareCrash;
+    public final Boolean hasWifiInfo;
+    public final Boolean hasDust;
+
     public SenseLogDocument(final String senseId, final Long timestamp, final String content, final String origin) {
         this.senseId = senseId;
         this.timestamp = timestamp;
         this.content = content;
         this.origin =  origin;
+        this.hasAlarm = content.matches(ALARM_RINGING_REGEX);
+        this.hasFirmwareCrash = content.matches(FIRMWARE_CRASH_REGEX);
+        this.hasWifiInfo = content.matches(WIFI_INFO_REGEX);
+        this.hasDust = content.matches(DUST_REGEX);
     }
     public Map<String, Object> toMap() {
         final ObjectMapper objectMapper = new ObjectMapper();
