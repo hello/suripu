54a3a2d37d85e8681a46239f19a2353c45391a41
Tim Bart <tim@sayhello.com>
2015-07-11 21:38:27 -0700

    Adding FW version headers from Sense. Not logged yet

--- suripu-core/src/main/java/com/hello/suripu/core/util/HelloHttpHeader.java
+++ suripu-core/src/main/java/com/hello/suripu/core/util/HelloHttpHeader.java
@@ -7,4 +7,6 @@ public class HelloHttpHeader {
     public static final String SENSE_ID = "X-Hello-Sense-Id";
     public static final String ADMIN = "X-Hello-Admin";
     public static final String ZENDESK = "X-Hello-Zendesk";
+    public static final String MIDDLE_FW_VERSION = "X-Hello-Sense-MFW";
+    public static final String TOP_FW_VERSION = "X-Hello-Sense-TFW";
 }
