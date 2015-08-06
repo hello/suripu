128520dd1e3e08a225ee830a21c8e5df1bf49e06
longd3 <izzy3ds@gmail.com>
2015-08-06 16:20:46 -0700

    Paraphrase default calibration

--- suripu-core/src/main/java/com/hello/suripu/core/models/Calibration.java
+++ suripu-core/src/main/java/com/hello/suripu/core/models/Calibration.java
@@ -4,29 +4,22 @@ package com.hello.suripu.core.models;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Calibration {
-    private final static Integer DEFAULT_DUST_OFFSET_VALUE = -1;
+    private final static Integer DEFAULT_DUST_CALIBRATION_DELTA = 0;
     private final static Float DUST_CALIBRATION_BASE = 300f;
     private final static Float DUST_CALIBRATION_K_FACTOR = 1.5f;
 
     @JsonProperty("sense_id")
     public final String senseId;
 
-    @JsonProperty("dust_offset")
-    public final Integer dustOffset;
+    @JsonProperty("dust_calibration_delta")
+    public final Integer dustCalibrationDelta;
 
     public Calibration(final String senseId, final Integer dustOffset) {
         this.senseId = senseId;
-        this.dustOffset = dustOffset;
+        this.dustCalibrationDelta = Math.round(DUST_CALIBRATION_BASE - dustOffset * DUST_CALIBRATION_K_FACTOR);
     }
 
     public static final Calibration createDefault(final String senseId) {
-        return new Calibration(senseId, DEFAULT_DUST_OFFSET_VALUE);
-    }
-
-    public final int getCalibratedDustCount(final Integer rawDust) {
-        if (this.dustOffset == DEFAULT_DUST_OFFSET_VALUE) {
-            return rawDust;
-        }
-        return Math.round(rawDust + (DUST_CALIBRATION_BASE - this.dustOffset * DUST_CALIBRATION_K_FACTOR));
+        return new Calibration(senseId, DEFAULT_DUST_CALIBRATION_DELTA);
     }
 }
