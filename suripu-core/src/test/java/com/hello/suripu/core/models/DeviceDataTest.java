4079096991b8541c883c31aeef9b74bfff179bf4
longd3 <izzy3ds@gmail.com>
2015-08-11 13:43:33 -0700

    Update current tests so that they don't fail
    Still need to write new tests with calibration taken into account

--- suripu-core/src/test/java/com/hello/suripu/core/models/DeviceDataTest.java
+++ suripu-core/src/test/java/com/hello/suripu/core/models/DeviceDataTest.java
@@ -29,7 +29,7 @@ public class DeviceDataTest {
                 .withAccountId(accountId)
                 .withDeviceId(251278L)
                 .withAmbientTemperature(temp)
-                .withAmbientAirQuality(dust, 1)
+                .withAmbientAirQuality(dust)
                 .withAmbientAirQualityRaw(dust)
                 .withAmbientDustVariance(0)
                 .withAmbientDustMin(0)
@@ -43,7 +43,7 @@ public class DeviceDataTest {
 
         final DeviceData deviceData = builder.build();
 
-        assertThat(deviceData.ambientAirQuality, is(64924));
+        assertThat(deviceData.ambientAirQuality, is(1000));
     }
 }
 
--- suripu-core/src/test/java/com/hello/suripu/core/util/DataUtilsTest.java
+++ suripu-core/src/test/java/com/hello/suripu/core/util/DataUtilsTest.java
@@ -1,5 +1,6 @@
 package com.hello.suripu.core.util;
 
+import com.hello.suripu.core.models.Calibration;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,9 +17,9 @@ public class DataUtilsTest {
     @Test
     public void testDustConversion() {
         final int values[] = new int[] {1179, 1570};
-        final int correct[] = new int[] {171, 211};
+        final int correct[] = new int[] {254, 322};
         for (int i = 0; i < values.length; i++) {
-            final int AQI = DataUtils.convertRawDustCountsToAQI(values[i], 1);
+            final int AQI = DataUtils.convertRawDustCountsToAQIWithCalibration(values[i], Calibration.createDefault("dummy-sense"), 1);
             LOGGER.debug("value {} -> {}", values[i], AQI);
             assertThat(AQI, is(correct[i]));
         }
