dfe49a162bbba0db4726f6625cf3474d4941be8e
longd3 <izzy3ds@gmail.com>
2015-08-10 18:20:38 -0700

    Fix typo in method name

--- suripu-app/src/main/java/com/hello/suripu/app/cli/CreateDynamoDBTables.java
+++ suripu-app/src/main/java/com/hello/suripu/app/cli/CreateDynamoDBTables.java
@@ -70,7 +70,7 @@ public class CreateDynamoDBTables extends ConfiguredCommand<SuripuAppConfigurati
         createFWUpgradePathTable(configuration, awsCredentialsProvider);
         createHmmBayesNetModelPriorTable(configuration,awsCredentialsProvider);
         createHmmBayesNetModelTable(configuration,awsCredentialsProvider);
-        createdCalibrationTable(configuration, awsCredentialsProvider);
+        createCalibrationTable(configuration, awsCredentialsProvider);
     }
 
     private void createSmartAlarmLogTable(final SuripuAppConfiguration configuration, final AWSCredentialsProvider awsCredentialsProvider){
@@ -452,7 +452,7 @@ public class CreateDynamoDBTables extends ConfiguredCommand<SuripuAppConfigurati
     }
 
 
-    private void createdCalibrationTable(final SuripuAppConfiguration configuration, final AWSCredentialsProvider awsCredentialsProvider) {
+    private void createCalibrationTable(final SuripuAppConfiguration configuration, final AWSCredentialsProvider awsCredentialsProvider) {
         final AmazonDynamoDBClient client = new AmazonDynamoDBClient(awsCredentialsProvider);
         final DynamoDBConfiguration config = configuration.getCalibrationConfiguration();
         final String tableName = config.getTableName();
