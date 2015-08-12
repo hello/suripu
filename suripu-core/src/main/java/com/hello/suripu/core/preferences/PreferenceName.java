0b0f47575f28fe2d54b170599588c249590aebcc
km <km@sayhello.com>
2015-08-12 16:12:42 -0700

    filter out new entries on v1 account preference resource

--- suripu-core/src/main/java/com/hello/suripu/core/preferences/PreferenceName.java
+++ suripu-core/src/main/java/com/hello/suripu/core/preferences/PreferenceName.java
@@ -1,19 +1,23 @@
 package com.hello.suripu.core.preferences;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.hello.suripu.core.models.ApiVersion;
 
 public enum PreferenceName {
-    ENHANCED_AUDIO("enhanced_audio"),
-    TEMP_CELSIUS("temp_celsius"),
-    TIME_TWENTY_FOUR_HOUR("time_twenty_four_hour"),
-    PUSH_SCORE("push_score"),
-    PUSH_ALERT_CONDITIONS("push_alert_conditions"),
-    WEIGHT_METRIC("weight_metric"),
-    HEIGHT_METRIC("height_metric");
+    ENHANCED_AUDIO(ApiVersion.V1, "enhanced_audio"),
+    TEMP_CELSIUS(ApiVersion.V1, "temp_celsius"),
+    TIME_TWENTY_FOUR_HOUR(ApiVersion.V1, "time_twenty_four_hour"),
+    PUSH_SCORE(ApiVersion.V1, "push_score"),
+    PUSH_ALERT_CONDITIONS(ApiVersion.V1, "push_alert_conditions"),
+    WEIGHT_METRIC(ApiVersion.V2, "weight_metric"),
+    HEIGHT_METRIC(ApiVersion.V2, "height_metric");
 
+    public final ApiVersion availableStarting;
     private String value;
 
-    PreferenceName(final String value) {
+    PreferenceName(final ApiVersion availableStarting,
+                   final String value) {
+        this.availableStarting = availableStarting;
         this.value = value;
     }
 
