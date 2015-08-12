2c29951b8b3b205b0032f138872e080e671c374e
Jingyun <jingyunfan@gmail.com>
2015-08-12 16:26:40 -0700

    Updating trigger for only Saturday - for final deploy

--- suripu-core/src/main/java/com/hello/suripu/core/processors/InsightProcessor.java
+++ suripu-core/src/main/java/com/hello/suripu/core/processors/InsightProcessor.java
@@ -185,13 +185,7 @@ public class InsightProcessor {
         InsightCard.Category categoryToGenerate;
 
         switch (dayOfWeek) {
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            case 5:
             case 6:
-            case 7:
                 if (!featureFlipper.userFeatureActive(FeatureFlipper.INSIGHTS_WAKE_VARIANCE, accountId, Collections.EMPTY_LIST)) {
                     return;
                 }
