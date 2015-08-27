8df12c3b89049d3b82fcfd0076a8390dcaef9c6a
Jingyun <jingyunfan@gmail.com>
2015-08-27 13:54:03 -0700

    remove system print out (this is for me to manually check that String formatting doesn't have any spacing, etc. mistakes

--- suripu-core/src/test/java/com/hello/suripu/core/processors/SleepMotionTest.java
+++ suripu-core/src/test/java/com/hello/suripu/core/processors/SleepMotionTest.java
@@ -41,7 +41,7 @@ public class SleepMotionTest {
         }
 
         final Optional<InsightCard> optionalCard = SleepMotion.processData(accountId, ImmutableList.copyOf(aggSleepStats), false);
-        System.out.print(optionalCard.get().message + "\n");
+//        System.out.print(optionalCard.get().message + "\n");
         if (optionalCard.isPresent()) {
             final Text expected = SleepMotionMsgEN.lessMovement(10, 10, 10);
             assertThat(optionalCard.get().title, is(expected.title));
@@ -64,7 +64,7 @@ public class SleepMotionTest {
         }
 
         final Optional<InsightCard> optionalCard = SleepMotion.processData(accountId, ImmutableList.copyOf(aggSleepStats), false);
-        System.out.print(optionalCard.get().message + "\n");
+//        System.out.print(optionalCard.get().message + "\n");
         if (optionalCard.isPresent()) {
             final Text expected = SleepMotionMsgEN.moreMovement(10, 10, 10);
             assertThat(optionalCard.get().title, is(expected.title));
@@ -87,7 +87,7 @@ public class SleepMotionTest {
         }
 
         final Optional<InsightCard> optionalCard = SleepMotion.processData(accountId, ImmutableList.copyOf(aggSleepStats), false);
-        System.out.print(optionalCard.get().message + "\n");
+//        System.out.print(optionalCard.get().message + "\n");
         if (optionalCard.isPresent()) {
             final Text expected = SleepMotionMsgEN.equalMovement(10, 10, 10);
             assertThat(optionalCard.get().title, is(expected.title));
@@ -111,7 +111,7 @@ public class SleepMotionTest {
         }
 
         final Optional<InsightCard> optionalCard = SleepMotion.processData(accountId, ImmutableList.copyOf(aggSleepStats), false);
-        System.out.print(optionalCard.get().message + "\n");
+//        System.out.print(optionalCard.get().message + "\n");
         if (optionalCard.isPresent()) {
             final Text expected = SleepMotionMsgEN.reallyEqualMovement(10, 10, 10);
             assertThat(optionalCard.get().title, is(expected.title));
