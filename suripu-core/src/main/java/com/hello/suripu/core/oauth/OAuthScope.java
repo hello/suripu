76d9b0f1467ebc901888be96c59aec5c597b381f
longd3 <izzy3ds@gmail.com>
2015-09-15 14:58:39 -0700

    Device with wifi info API @ DeviceResource v2

--- suripu-core/src/main/java/com/hello/suripu/core/oauth/OAuthScope.java
+++ suripu-core/src/main/java/com/hello/suripu/core/oauth/OAuthScope.java
@@ -40,7 +40,9 @@ public enum OAuthScope {
     IMPLICIT_TOKEN(29),
     ZENDESK_EXTENSION(30),
     SUPPORT(31),
-    STORE_FEEDBACK(32);
+    STORE_FEEDBACK(32),
+    WIFI_INFO_READ(33),
+    WIFI_INFO_WRITE(34);
 
     private int value;
 
