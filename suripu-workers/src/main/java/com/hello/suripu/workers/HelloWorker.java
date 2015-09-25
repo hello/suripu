d25afb02a7acf583636ffbddaf2af8937aa71604
longd3 <izzy3ds@gmail.com>
2015-09-25 16:44:49 -0700

    Remove kinesis logger from ALL workers

--- suripu-workers/src/main/java/com/hello/suripu/workers/HelloWorker.java
+++ suripu-workers/src/main/java/com/hello/suripu/workers/HelloWorker.java
@@ -1,7 +1,5 @@
 package com.hello.suripu.workers;
 
-import com.hello.suripu.coredw.bundles.KinesisLoggerBundle;
-import com.hello.suripu.core.configuration.KinesisLoggerConfiguration;
 import com.hello.suripu.workers.alarm.AlarmWorkerCommand;
 import com.hello.suripu.workers.framework.WorkerConfiguration;
 import com.hello.suripu.workers.insights.InsightsGeneratorWorkerCommand;
@@ -27,12 +25,6 @@ public class HelloWorker extends Service<WorkerConfiguration> {
 
     @Override
     public void initialize(Bootstrap<WorkerConfiguration> bootstrap) {
-        bootstrap.addBundle(new KinesisLoggerBundle<WorkerConfiguration>() {
-            @Override
-            public KinesisLoggerConfiguration getConfiguration(WorkerConfiguration configuration) {
-                return configuration.getKinesisLoggerConfiguration();
-            }
-        });
         bootstrap.addCommand(new PillWorkerCommand("pill", "all things about pill"));
         bootstrap.addCommand(new SenseSaveWorkerCommand("sense_save", "saving sense sensor data"));
         bootstrap.addCommand(new AlarmWorkerCommand("smart_alarm", "Start smart alarm worker"));
