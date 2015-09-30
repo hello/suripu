894279b38721bc543d1f07569ff44300e84cd2c7
jakepic1 <jake@sayhello.com>
2015-09-30 10:45:39 -0700

    Add metrics to TimelineLogProcessor

--- suripu-workers/src/main/java/com/hello/suripu/workers/logs/timeline/TimelineLogProcessor.java
+++ suripu-workers/src/main/java/com/hello/suripu/workers/logs/timeline/TimelineLogProcessor.java
@@ -9,20 +9,48 @@ import com.google.common.collect.Sets;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.hello.suripu.api.logging.LoggingProtos;
 import com.hello.suripu.workers.framework.HelloBaseRecordProcessor;
+import com.yammer.metrics.Metrics;
+import com.yammer.metrics.core.Meter;
+import com.yammer.metrics.util.RatioGauge;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class TimelineLogProcessor extends HelloBaseRecordProcessor {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(TimelineLogProcessor.class);
     private final TimelineAnalytics timelineAnalytics;
 
+    private final Meter timelineLogsReceived;
+    private final Meter successfulTimelineLogInsertions;
+    private final RatioGauge ratioOfSuccessfulInsertionsToReceived;
+
     public TimelineLogProcessor(final TimelineAnalytics timelineAnalytics) {
         this.timelineAnalytics = timelineAnalytics;
+        this.timelineLogsReceived = Metrics.defaultRegistry()
+                .newMeter(TimelineLogProcessor.class, "received", "timeline-logs", TimeUnit.SECONDS);
+        this.successfulTimelineLogInsertions= Metrics.defaultRegistry()
+                .newMeter(TimelineLogProcessor.class, "inserted", "timeline-logs", TimeUnit.SECONDS);
+        this.ratioOfSuccessfulInsertionsToReceived = new RatioGauge() {
+            @Override
+            protected double getNumerator() {
+                return successfulTimelineLogInsertions.oneMinuteRate();
+            }
+
+            @Override
+            protected double getDenominator() {
+                return timelineLogsReceived.oneMinuteRate();
+            }
+        };
+        Metrics.defaultRegistry().newGauge(
+                TimelineLogProcessor.class,
+                "inserted-per-received",
+                "timeline-logs",
+                ratioOfSuccessfulInsertionsToReceived);
     }
 
     public static TimelineLogProcessor create(final TimelineAnalytics timelineAnalytics) {
@@ -55,7 +83,10 @@ public class TimelineLogProcessor extends HelloBaseRecordProcessor {
             }
         }
 
-        timelineAnalytics.insertBatchWithIndividualRetry(uniqueLogs);
+        timelineLogsReceived.mark(uniqueLogs.size());
+
+        final int successfulInsertions = timelineAnalytics.insertBatchWithIndividualRetry(uniqueLogs);
+        successfulTimelineLogInsertions.mark(successfulInsertions);
 
         try {
             checkpointer.checkpoint();
