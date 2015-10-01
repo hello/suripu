de1551ca01a0a1244be399a0bf7d17084dba90fb
Tim Bart <tim@sayhello.com>
2015-10-01 16:54:51 -0700

    It is SIX months NOT FIVE

--- suripu-core/src/test/java/com/hello/suripu/core/util/DateTimeUtilTest.java
+++ suripu-core/src/test/java/com/hello/suripu/core/util/DateTimeUtilTest.java
@@ -49,7 +49,7 @@ public class DateTimeUtilTest {
     public void testSanitizeDateTimeSenseThinksIts2016() {
         // This attempts to simulate the bug where Sense is off by 5 months
         final DateTime ref = DateTime.now(DateTimeZone.UTC).withTimeAtStartOfDay();
-        final DateTime sampleTime = ref.plusMonths(5);
+        final DateTime sampleTime = ref.plusMonths(DateTimeUtil.MONTH_OFFSET_FOR_CLOCK_BUG);
         assertThat(DateTimeUtil.possiblySanitizeSampleTime(ref, sampleTime, 2), equalTo(ref));
     }
 
