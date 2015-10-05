91cff76a69284f01e5d066a251f1edfe7df77d2f
Jingyun <jingyunfan@gmail.com>
2015-10-05 14:02:03 -0700

    added local_utc_ts limit to speed up query.

--- suripu-core/src/main/java/com/hello/suripu/core/db/DeviceDataDAO.java
+++ suripu-core/src/main/java/com/hello/suripu/core/db/DeviceDataDAO.java
@@ -185,25 +185,31 @@ public abstract class DeviceDataDAO {
     @SqlQuery("SELECT * FROM device_sensors_master " +
             "WHERE account_id = :account_id AND device_id = :device_id " +
             "AND ts >= :start_ts AND ts <= :end_ts " +
+            "AND local_utc_ts >= :start_local_utc_ts AND local_utc_ts <= :end_local_utc_ts " +
             "AND (CAST(date_part('hour', local_utc_ts) AS INTEGER) >= :start_hour " +
             "AND CAST(date_part('hour', local_utc_ts) AS INTEGER) < :end_hour)")
     public abstract ImmutableList<DeviceData> getBetweenHourDateByTSSameDay(@Bind("account_id") Long accountId,
-                                                                     @Bind("device_id") Long deviceId,
-                                                                     @Bind("start_ts") DateTime startTimestamp,
-                                                                     @Bind("end_ts") DateTime endTimestamp,
-                                                                     @Bind("start_hour") int startHour,
-                                                                     @Bind("end_hour") int endHour);
+                                                                            @Bind("device_id") Long deviceId,
+                                                                            @Bind("start_ts") DateTime startTimestamp,
+                                                                            @Bind("end_ts") DateTime endTimestamp,
+                                                                            @Bind("start_local_utc_ts") DateTime startLocalTimeStamp,
+                                                                            @Bind("end_local_utc_ts") DateTime endLocalTimeStamp,
+                                                                            @Bind("start_hour") int startHour,
+                                                                            @Bind("end_hour") int endHour);
 
     @RegisterMapper(DeviceDataMapper.class)
     @SqlQuery("SELECT * FROM device_sensors_master " +
             "WHERE account_id = :account_id AND device_id = :device_id " +
             "AND ts >= :start_ts AND ts <= :end_ts " +
+            "AND local_utc_ts >= :start_local_utc_ts AND local_utc_ts <= :end_local_utc_ts " +
             "AND (CAST(date_part('hour', local_utc_ts) AS INTEGER) >= :start_hour " +
             "OR CAST(date_part('hour', local_utc_ts) AS INTEGER) < :end_hour)")
     public abstract ImmutableList<DeviceData> getBetweenHourDateByTS(@Bind("account_id") Long accountId,
                                                                      @Bind("device_id") Long deviceId,
                                                                      @Bind("start_ts") DateTime startTimestamp,
                                                                      @Bind("end_ts") DateTime endTimestamp,
+                                                                     @Bind("start_local_utc_ts") DateTime startLocalTimeStamp,
+                                                                     @Bind("end_local_utc_ts") DateTime endLocalTimeStamp,
                                                                      @Bind("start_hour") int startHour,
                                                                      @Bind("end_hour") int endHour);
 
