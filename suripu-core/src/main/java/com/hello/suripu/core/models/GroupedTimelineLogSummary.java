d1ce1bfb94a1648eecc3da2a80b31f04096ff665
jakepic1 <jake@sayhello.com>
2015-10-06 16:02:56 -0700

    Add JsonProperties to GroupedTimelineLogSummary

--- suripu-core/src/main/java/com/hello/suripu/core/models/GroupedTimelineLogSummary.java
+++ suripu-core/src/main/java/com/hello/suripu/core/models/GroupedTimelineLogSummary.java
@@ -1,13 +1,20 @@
 package com.hello.suripu.core.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.hello.suripu.api.logging.LoggingProtos;
 
 /**
  * Created by jakepiccolo on 10/5/15.
  */
 public class GroupedTimelineLogSummary {
+
+    @JsonProperty("algorithm")
     public final LoggingProtos.TimelineLog.AlgType algorithm;
+
+    @JsonProperty("error")
     public final LoggingProtos.TimelineLog.ErrorType error;
+
+    @JsonProperty("count")
     public final int count;
 
     public GroupedTimelineLogSummary(final LoggingProtos.TimelineLog.AlgType algorithm,
