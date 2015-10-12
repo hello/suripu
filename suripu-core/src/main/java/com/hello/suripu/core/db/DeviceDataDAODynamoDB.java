4dcbe90d5ef80d3cc32093b94c07004a70ec601d
jakepic1 <jake@sayhello.com>
2015-10-12 11:12:35 -0700

    WIP! Add new device data DynamoDB DAO

---
+++ suripu-core/src/main/java/com/hello/suripu/core/db/DeviceDataDAODynamoDB.java
@@ -0,0 +1,314 @@
+package com.hello.suripu.core.db;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.BatchWriteItemRequest;
+import com.amazonaws.services.dynamodbv2.model.BatchWriteItemResult;
+import com.amazonaws.services.dynamodbv2.model.ComparisonOperator;
+import com.amazonaws.services.dynamodbv2.model.Condition;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.CreateTableResult;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
+import com.amazonaws.services.dynamodbv2.model.KeyType;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughputExceededException;
+import com.amazonaws.services.dynamodbv2.model.PutItemResult;
+import com.amazonaws.services.dynamodbv2.model.PutRequest;
+import com.amazonaws.services.dynamodbv2.model.QueryRequest;
+import com.amazonaws.services.dynamodbv2.model.QueryResult;
+import com.amazonaws.services.dynamodbv2.model.WriteRequest;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import com.hello.suripu.api.logging.LoggingProtos;
+import com.hello.suripu.core.models.DeviceData;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by jakepiccolo on 10/9/15.
+ */
+public class DeviceDataDAODynamoDB {
+    private final static Logger LOGGER = LoggerFactory.getLogger(DeviceDataDAODynamoDB.class);
+
+    private final AmazonDynamoDB dynamoDBClient;
+    // TODO should have ThreadPoolExecutor to parallelize batchInsert?
+    private final String tableName;
+
+    public static final class AttributeNames {
+        public static final String DEVICE_ID                = "device_id";
+        public static final String TIMESTAMP                = "timestamp";
+        public static final String ACCOUNT_ID               = "account_id";
+        public static final String AMBIENT_TEMP             = "ambient_temp";
+        public static final String AMBIENT_LIGHT            = "ambient_light";
+        public static final String AMBIENT_LIGHT_VARIANCE   = "ambient_light_variance";
+        public static final String AMBIENT_LIGHT_PEAKINESS  = "ambient_light_peakiness";
+        public static final String AMBIENT_HUMIDITY         = "ambient_humidity";
+        public static final String AMBIENT_AIR_QUALITY      = "ambient_air_quality";
+        public static final String AMBIENT_AIR_QUALITY_RAW  = "ambient_air_quality_raw";
+        public static final String AMBIENT_DUST_VARIANCE    = "ambient_dust_variance";
+        public static final String AMBIENT_DUST_MIN         = "ambient_dust_min";
+        public static final String AMBIENT_DUST_MAX         = "ambient_dust_max";
+        public static final String LOCAL_UTC_TIMESTAMP      = "local_utc_ts";
+        public static final String OFFSET_MILLIS            = "offset_millis";
+    }
+
+    private static final int MAX_GET_ITEMS = 100;   // Doesn't apply to Query requests, only BatchGetItem
+    private static final int MAX_PUT_ITEMS = 25;
+    public static final int DEFAULT_MAX_WRITE_ATTEMPTS = 3;
+    public static final int DEFAULT_MAX_QUERY_ATTEMPTS = 3;
+
+    private static final String RANGE_KEY_NAME = "account_id:timestamp";
+
+    // Store everything to the minute level
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm");
+
+    private static final ImmutableMap<String, String> ATTRIBUTE_TYPES = new ImmutableMap.Builder<String, String>()
+            .put(AttributeNames.DEVICE_ID, "N")
+            .put(AttributeNames.TIMESTAMP, "S")
+            .put(AttributeNames.ACCOUNT_ID, "N")
+            .put(AttributeNames.AMBIENT_TEMP, "N")
+            .put(AttributeNames.AMBIENT_LIGHT, "N")
+            .put(AttributeNames.AMBIENT_LIGHT_VARIANCE, "N")
+            .put(AttributeNames.AMBIENT_LIGHT_PEAKINESS, "N")
+            .put(AttributeNames.AMBIENT_HUMIDITY, "N")
+            .put(AttributeNames.AMBIENT_AIR_QUALITY, "N")
+            .put(AttributeNames.AMBIENT_AIR_QUALITY_RAW, "N")
+            .put(AttributeNames.AMBIENT_DUST_VARIANCE, "N")
+            .put(AttributeNames.AMBIENT_DUST_MIN, "N")
+            .put(AttributeNames.AMBIENT_DUST_MAX, "N")
+            .put(AttributeNames.LOCAL_UTC_TIMESTAMP, "S")
+            .put(AttributeNames.OFFSET_MILLIS, "N")
+            .build();
+
+
+    public DeviceDataDAODynamoDB(final AmazonDynamoDB dynamoDBClient, final String tableName) {
+        this.dynamoDBClient = dynamoDBClient;
+        this.tableName = tableName;
+    }
+
+    public static CreateTableResult createTable(final String tableName, final AmazonDynamoDB dynamoDBClient) {
+        // attributes
+        ArrayList<AttributeDefinition> attributes = new ArrayList<>();
+        attributes.add(new AttributeDefinition().withAttributeName(AttributeNames.DEVICE_ID).withAttributeType("N"));
+        attributes.add(new AttributeDefinition().withAttributeName(RANGE_KEY_NAME).withAttributeType("S"));
+
+        // keys
+        ArrayList<KeySchemaElement> keySchema = new ArrayList<>();
+        keySchema.add(new KeySchemaElement().withAttributeName(AttributeNames.DEVICE_ID).withKeyType(KeyType.HASH));
+        keySchema.add(new KeySchemaElement().withAttributeName(RANGE_KEY_NAME).withKeyType(KeyType.RANGE));
+
+        // throughput provision
+        ProvisionedThroughput provisionedThroughput = new ProvisionedThroughput()
+                .withReadCapacityUnits(1L)
+                .withWriteCapacityUnits(1L);
+
+        final CreateTableRequest request = new CreateTableRequest()
+                .withTableName(tableName)
+                .withAttributeDefinitions(attributes)
+                .withKeySchema(keySchema)
+                .withProvisionedThroughput(provisionedThroughput);
+
+        return dynamoDBClient.createTable(request);
+    }
+
+    private AttributeValue dateTimeToAttributeValue(final DateTime dateTime) {
+        return new AttributeValue(dateTime.toString(DATE_TIME_FORMATTER));
+    }
+
+    private AttributeValue getRangeKey(final Long accountId, final DateTime dateTime) {
+        return new AttributeValue(String.valueOf(accountId) + ":" + dateTime.toString(DATE_TIME_FORMATTER));
+    }
+
+    private HashMap<String, AttributeValue> deviceDataToAttributeMap(final DeviceData data) {
+        final HashMap<String, AttributeValue> item = new HashMap<>();
+        item.put(AttributeNames.DEVICE_ID, new AttributeValue().withN(String.valueOf(data.deviceId)));
+        item.put(RANGE_KEY_NAME, getRangeKey(data.accountId, data.dateTimeUTC));
+        item.put(AttributeNames.TIMESTAMP, dateTimeToAttributeValue(data.dateTimeUTC));
+        item.put(AttributeNames.ACCOUNT_ID, new AttributeValue().withN(String.valueOf(data.accountId)));
+        item.put(AttributeNames.AMBIENT_TEMP, new AttributeValue().withN(String.valueOf(data.ambientTemperature)));
+        item.put(AttributeNames.AMBIENT_LIGHT, new AttributeValue().withN(String.valueOf(data.ambientLight)));
+        item.put(AttributeNames.AMBIENT_LIGHT_VARIANCE, new AttributeValue().withN(String.valueOf(data.ambientLightVariance)));
+        item.put(AttributeNames.AMBIENT_LIGHT_PEAKINESS, new AttributeValue().withN(String.valueOf(data.ambientLightPeakiness)));
+        item.put(AttributeNames.AMBIENT_HUMIDITY, new AttributeValue().withN(String.valueOf(data.ambientHumidity)));
+        item.put(AttributeNames.AMBIENT_AIR_QUALITY, new AttributeValue().withN(String.valueOf(data.ambientAirQuality)));
+        item.put(AttributeNames.AMBIENT_AIR_QUALITY_RAW, new AttributeValue().withN(String.valueOf(data.ambientAirQualityRaw)));
+        item.put(AttributeNames.AMBIENT_DUST_VARIANCE, new AttributeValue().withN(String.valueOf(data.ambientDustVariance)));
+        item.put(AttributeNames.AMBIENT_DUST_MIN, new AttributeValue().withN(String.valueOf(data.ambientDustMin)));
+        item.put(AttributeNames.AMBIENT_DUST_MAX, new AttributeValue().withN(String.valueOf(data.ambientDustMax)));
+        item.put(AttributeNames.LOCAL_UTC_TIMESTAMP, dateTimeToAttributeValue(data.dateTimeUTC.plusMillis(data.offsetMillis)));
+        item.put(AttributeNames.OFFSET_MILLIS, new AttributeValue().withN(String.valueOf(data.offsetMillis)));
+        return item;
+    }
+
+    private DeviceData attributeMapToDeviceData(final Map<String, AttributeValue> item) {
+        return new DeviceData.Builder()
+                .withDeviceId(Long.valueOf(item.get(AttributeNames.DEVICE_ID).getN()))
+                .withDateTimeUTC(DateTime.parse(item.get(AttributeNames.TIMESTAMP).getS(), DATE_TIME_FORMATTER))
+                .withAccountId(Long.valueOf(item.get(AttributeNames.ACCOUNT_ID).getN()))
+                .withAmbientTemperature(Integer.valueOf(item.get(AttributeNames.AMBIENT_TEMP).getN()))
+                .withAmbientLight(Integer.valueOf(item.get(AttributeNames.AMBIENT_LIGHT).getN()))
+                .withAmbientLightVariance(Integer.valueOf(item.get(AttributeNames.AMBIENT_LIGHT_VARIANCE).getN()))
+                .withAmbientLightPeakiness(Integer.valueOf(item.get(AttributeNames.AMBIENT_LIGHT_PEAKINESS).getN()))
+                .withAmbientHumidity(Integer.valueOf(item.get(AttributeNames.AMBIENT_HUMIDITY).getN()))
+                .withAmbientAirQualityRaw(Integer.valueOf(item.get(AttributeNames.AMBIENT_AIR_QUALITY_RAW).getN()))
+                .withAmbientDustVariance(Integer.valueOf(item.get(AttributeNames.AMBIENT_DUST_VARIANCE).getN()))
+                .withAmbientDustMin(Integer.valueOf(item.get(AttributeNames.AMBIENT_DUST_MIN).getN()))
+                .withAmbientDustMax(Integer.valueOf(item.get(AttributeNames.AMBIENT_DUST_MAX).getN()))
+                .withOffsetMillis(Integer.valueOf(item.get(AttributeNames.OFFSET_MILLIS).getN()))
+                .build();
+    }
+
+
+    public void insert(final DeviceData deviceData) {
+        final HashMap<String, AttributeValue> item = deviceDataToAttributeMap(deviceData);
+        dynamoDBClient.putItem(tableName, item);
+    }
+
+    /**
+     * Returns the number of items that failed to insert
+     * @param deviceDataList
+     * @return
+     */
+    public int batchInsert(final List<DeviceData> deviceDataList) {
+        final List<List<DeviceData>> deviceDataLists = Lists.partition(deviceDataList, MAX_PUT_ITEMS);
+        int failedItemsCount = 0;
+
+        // Insert each chunk
+        for (final List<DeviceData> deviceDataListToWrite: deviceDataLists) {
+            final List<WriteRequest> writeRequestList = new LinkedList<>();
+            for (final DeviceData data : deviceDataListToWrite) {
+                final HashMap<String, AttributeValue> item = deviceDataToAttributeMap(data);
+                writeRequestList.add(new WriteRequest().withPutRequest(new PutRequest().withItem(item)));
+            }
+
+            Map<String, List<WriteRequest>> requestItems = new HashMap<>();
+            requestItems.put(this.tableName, writeRequestList);
+
+            do {
+                final BatchWriteItemRequest batchWriteItemRequest = new BatchWriteItemRequest().withRequestItems(requestItems);
+                final BatchWriteItemResult result = this.dynamoDBClient.batchWriteItem(batchWriteItemRequest);
+                // check for unprocessed items
+                requestItems = result.getUnprocessedItems();
+                LOGGER.debug("Unprocessed put request count {}", requestItems.size());
+
+            } while (requestItems.size() > 0);
+
+            if (!requestItems.isEmpty()) {
+                failedItemsCount += requestItems.get(tableName).size();
+            }
+        }
+
+        return deviceDataList.size() - failedItemsCount;
+    }
+
+    public int batchInsertWithFailureFallback(final List<DeviceData> deviceDataList) {
+        final List<List<DeviceData>> deviceDataLists = Lists.partition(deviceDataList, MAX_PUT_ITEMS);
+        int successfulInsertions = 0;
+
+        // Insert each chunk
+        for (final List<DeviceData> deviceDataListToWrite: deviceDataLists) {
+            try {
+                batchInsert(deviceDataListToWrite);
+                successfulInsertions += deviceDataList.size();
+            } catch (AmazonClientException e) {
+                LOGGER.error("Got exception while attempting to batchInsert to DynamoDB: {}", e);
+
+                for (final DeviceData deviceData : deviceDataListToWrite) {
+                    try {
+                        insert(deviceData);
+                        successfulInsertions++;
+                    } catch (AmazonClientException ex) {
+                        LOGGER.error("Got exception while attempting to insert to DynamoDB: {}", ex);
+                    }
+                }
+            }
+
+        }
+
+        return successfulInsertions;
+    }
+
+    // TODO batchInsertWithFailureFallback
+
+    public ImmutableList<DeviceData> getBetweenByAbsoluteTimeAggregateBySlotDuration(
+            final Long deviceId,
+            final Long accountId,
+            final DateTime start,
+            final DateTime end,
+            // TODO this field is currently ignored, no aggregation ATM
+            final Integer slotDuration,
+            final Collection<String> targetAttributes)
+    {
+
+        final Condition selectByDeviceId  = new Condition()
+                .withComparisonOperator(ComparisonOperator.EQ)
+                .withAttributeValueList(new AttributeValue().withN(String.valueOf(deviceId)));
+
+        final Condition selectByTimestamp = new Condition()
+                .withComparisonOperator(ComparisonOperator.BETWEEN.toString())
+                .withAttributeValueList(
+                        getRangeKey(accountId, start),
+                        getRangeKey(accountId, end));
+
+        final Map<String, Condition> queryConditions = new HashMap<>();
+        queryConditions.put(AttributeNames.DEVICE_ID, selectByDeviceId);
+        queryConditions.put(RANGE_KEY_NAME, selectByTimestamp);
+
+        final List<DeviceData> results = new ArrayList<>();
+
+        Map<String, AttributeValue> lastEvaluatedKey = null;
+        int numAttempts = 0;
+
+        do {
+            final QueryRequest queryRequest = new QueryRequest()
+                    .withTableName(this.tableName)
+                    .withKeyConditions(queryConditions)
+                    .withAttributesToGet(targetAttributes)
+                    .withExclusiveStartKey(lastEvaluatedKey);
+
+            final QueryResult queryResult = this.dynamoDBClient.query(queryRequest);
+            final List<Map<String, AttributeValue>> items = queryResult.getItems();
+
+            if (queryResult.getItems() != null) {
+                for (final Map<String, AttributeValue> item : items) {
+                    if (!item.keySet().containsAll(targetAttributes)) {
+                        LOGGER.warn("Missing field in item {}", item);
+                        continue;
+                    }
+                    results.add(this.attributeMapToDeviceData(item));
+                }
+            }
+
+            lastEvaluatedKey = queryResult.getLastEvaluatedKey();
+            numAttempts++;
+
+        } while (lastEvaluatedKey != null && numAttempts < DEFAULT_MAX_QUERY_ATTEMPTS);
+
+        return ImmutableList.copyOf(results);
+    }
+
+
+    public ImmutableList<DeviceData> getBetweenByAbsoluteTimeAggregateBySlotDuration(
+            final Long deviceId,
+            final Long accountId,
+            final DateTime start,
+            final DateTime end,
+            final Integer slotDuration)
+    {
+        return getBetweenByAbsoluteTimeAggregateBySlotDuration(deviceId, accountId, start, end, slotDuration, ATTRIBUTE_TYPES.keySet());
+    }
+}
---
+++ suripu-core/src/test/java/com/hello/suripu/core/db/DeviceDataDAODynamoDBIT.java
@@ -0,0 +1,161 @@
+package com.hello.suripu.core.db;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.model.CreateTableResult;
+import com.amazonaws.services.dynamodbv2.model.DeleteTableRequest;
+import com.amazonaws.services.dynamodbv2.model.ResourceInUseException;
+import com.amazonaws.services.dynamodbv2.model.ResourceNotFoundException;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
+import com.google.common.collect.ImmutableList;
+import com.hello.suripu.core.models.Device;
+import com.hello.suripu.core.models.DeviceData;
+import org.joda.time.DateTime;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Created by jakepiccolo on 10/13/15.
+ */
+public class DeviceDataDAODynamoDBIT {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(InsightsDAODynamoDBIT.class);
+
+    private BasicAWSCredentials awsCredentials;
+    private AmazonDynamoDBClient amazonDynamoDBClient;
+    private DeviceDataDAODynamoDB deviceDataDAODynamoDB;
+
+    private static final String TABLE_NAME = "integration_test_device_data";
+
+
+    @Before
+    public void setUp() throws Exception {
+        this.awsCredentials = new BasicAWSCredentials("FAKE_AWS_KEY", "FAKE_AWS_SECRET");
+        final ClientConfiguration clientConfiguration = new ClientConfiguration();
+        clientConfiguration.setMaxErrorRetry(0);
+        this.amazonDynamoDBClient = new AmazonDynamoDBClient(this.awsCredentials, clientConfiguration);
+        this.amazonDynamoDBClient.setEndpoint("http://localhost:7777");
+
+        tearDown();
+
+        try {
+            LOGGER.debug("-------- Creating Table {} ---------", TABLE_NAME);
+            final CreateTableResult result = DeviceDataDAODynamoDB.createTable(TABLE_NAME, amazonDynamoDBClient);
+            LOGGER.debug("Created dynamoDB table {}", result.getTableDescription());
+        } catch (ResourceInUseException rie){
+            LOGGER.warn("Problem creating table");
+        }
+        this.deviceDataDAODynamoDB = new DeviceDataDAODynamoDB(amazonDynamoDBClient, TABLE_NAME);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        final DeleteTableRequest deleteTableRequest = new DeleteTableRequest()
+                .withTableName(TABLE_NAME);
+        try {
+            this.amazonDynamoDBClient.deleteTable(deleteTableRequest);
+        }catch (ResourceNotFoundException ex){
+            LOGGER.warn("Can not delete non existing table");
+        }
+    }
+
+    private int getTableCount() {
+        final ScanRequest scanRequest = new ScanRequest().withTableName(TABLE_NAME);
+        final ScanResult scanResult = amazonDynamoDBClient.scan(scanRequest);
+        return scanResult.getCount();
+    }
+
+    @Test
+    public void testBatchInsert() {
+        final List<DeviceData> deviceDataList = new ArrayList<>();
+        final List<Long> accountIds = new ImmutableList.Builder<Long>().add(new Long(1)).add(new Long(2)).build();
+        final Long deviceId = new Long(100);
+        final DateTime firstTime = new DateTime(2015, 10, 1, 7, 0);
+        final int numMinutes = 50;
+        for (final Long accountId: accountIds) {
+            for (int i = 0; i < numMinutes; i++) {
+                deviceDataList.add(new DeviceData.Builder()
+                        .withAccountId(accountId)
+                        .withDeviceId(deviceId)
+                        .withDateTimeUTC(firstTime.plusMinutes(i))
+                        .withOffsetMillis(0)
+                        .build());
+            }
+        }
+
+        final int initialItemsInserted = deviceDataDAODynamoDB.batchInsert(deviceDataList);
+        assertThat(initialItemsInserted, is(deviceDataList.size()));
+        assertThat(getTableCount(), is(deviceDataList.size()));
+
+        // Now insert the exact same thing again. Should work fine in DynamoDB.
+        final int duplicateItemsInserted = deviceDataDAODynamoDB.batchInsert(deviceDataList);
+        assertThat(duplicateItemsInserted, is(deviceDataList.size()));
+        assertThat(getTableCount(), is(deviceDataList.size()));
+    }
+
+    @Test(expected = AmazonServiceException.class)
+    public void testBatchInsertFailsDueToDuplicateKeys() {
+        final DeviceData.Builder builder = new DeviceData.Builder()
+                .withAccountId(new Long(0))
+                .withDeviceId(new Long(0))
+                .withDateTimeUTC(new DateTime(2015, 10, 1, 8, 0))
+                .withOffsetMillis(0);
+        final List<DeviceData> deviceDataList = new ImmutableList.Builder<DeviceData>()
+                .add(builder.build())
+                .add(builder.build())
+                .build();
+        deviceDataDAODynamoDB.batchInsert(deviceDataList);
+    }
+
+    @Test
+    public void testGetBetweenByAbsoluteTimeAggregateBySlotDuration() {
+        final List<DeviceData> deviceDataList = new ArrayList<>();
+        final Long accountId = new Long(1);
+        final Long deviceId = new Long(1);
+        final Integer offsetMillis = 0;
+        final DateTime firstTime = new DateTime(2015, 10, 1, 7, 0);
+        deviceDataList.add(new DeviceData.Builder()
+                .withAccountId(accountId)
+                .withDeviceId(deviceId)
+                .withDateTimeUTC(firstTime)
+                .withOffsetMillis(offsetMillis)
+                .build());
+        deviceDataList.add(new DeviceData.Builder()
+                .withAccountId(accountId)
+                .withDeviceId(deviceId)
+                .withDateTimeUTC(firstTime.plusMinutes(1))
+                .withOffsetMillis(offsetMillis)
+                .build());
+        deviceDataDAODynamoDB.batchInsert(deviceDataList);
+
+        // From start to start+1, 2 results
+        assertThat(deviceDataDAODynamoDB.getBetweenByAbsoluteTimeAggregateBySlotDuration(
+                        deviceId, accountId, firstTime, firstTime.plusMinutes(1), 1).size(),
+                is(2));
+        // From start to start+2, Also 2 results because only 2 in table
+        assertThat(deviceDataDAODynamoDB.getBetweenByAbsoluteTimeAggregateBySlotDuration(
+                        deviceId, accountId, firstTime, firstTime.plusMinutes(2), 1).size(),
+                is(2));
+        // from start to start should be 1 result
+        assertThat(deviceDataDAODynamoDB.getBetweenByAbsoluteTimeAggregateBySlotDuration(
+                        deviceId, accountId, firstTime, firstTime, 1).size(),
+                is(1));
+        // Account ID unrecognized should be 0 results
+        assertThat(deviceDataDAODynamoDB.getBetweenByAbsoluteTimeAggregateBySlotDuration(
+                        deviceId, accountId + 1000, firstTime, firstTime, 1).size(),
+                is(0));
+    }
+
+}
