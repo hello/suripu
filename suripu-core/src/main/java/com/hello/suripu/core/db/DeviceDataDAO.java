0e1d6e20bb11372fbd615538b1a86974ced0ceaa
Jingyun <jingyunfan@gmail.com>
2015-10-16 16:41:11 -0700

    new sql query. Low cost, see benchmark in PR comment

--- suripu-core/src/main/java/com/hello/suripu/core/db/DeviceDataDAO.java
+++ suripu-core/src/main/java/com/hello/suripu/core/db/DeviceDataDAO.java
@@ -186,6 +186,19 @@ public abstract class DeviceDataDAO implements DeviceDataIngestDAO {
                                                                             @Bind("start_hour") int startHour,
                                                                             @Bind("end_hour") int endHour);
 
+    @SqlQuery("SELECT AVG(ambient_air_quality), EXTRACT(day FROM local_utc_ts) AS date FROM device_sensors_master " +
+            "WHERE account_id = :account_id AND device_id = :device_id " +
+            "AND ts >= :start_ts AND ts <= :end_ts " +
+            "AND local_utc_ts >= :start_local_utc_ts AND local_utc_ts <= :end_local_utc_ts " +
+            "GROUP BY date")
+    public abstract ImmutableList<Float> getAirQualityList(@Bind("account_id") Long accountId,
+                                                             @Bind("device_id") Long deviceId,
+                                                             @Bind("start_ts") DateTime startTimestamp,
+                                                             @Bind("end_ts") DateTime endTimestamp,
+                                                             @Bind("start_local_utc_ts") DateTime startLocalTimeStamp,
+                                                             @Bind("end_local_utc_ts") DateTime endLocalTimeStamp);
+
+
     @RegisterMapper(DeviceDataMapper.class)
     @SqlQuery("SELECT * FROM device_sensors_master " +
             "WHERE account_id = :account_id AND device_id = :device_id " +
