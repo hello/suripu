495cc1a655b4c45d3f3fbab47051601813a655ab
kingshy <kingshy@sayhello.com>
2015-11-17 18:09:29 -0800

    rm unnecessary code, not sure how it got there

--- suripu-app/src/main/java/com/hello/suripu/app/configuration/SuripuAppConfiguration.java
+++ suripu-app/src/main/java/com/hello/suripu/app/configuration/SuripuAppConfiguration.java
@@ -22,10 +22,6 @@ public class SuripuAppConfiguration extends Configuration {
     @NotNull
     @JsonProperty("sensors_db")
     private DatabaseConfiguration sensorsDB = new DatabaseConfiguration();
-
-    public SuripuAppConfiguration() {
-    }
-
     public DatabaseConfiguration getSensorsDB() {
         return sensorsDB;
     }
