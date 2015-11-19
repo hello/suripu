f2e07a7d6da17a457831408d05844a4830197cb0
Josef Norgan <josef@sayhello.com>
2015-11-19 13:14:12 -0800

    Removed suripu-research from suripu-parent

--- research_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-git checkout research
-git pull
-mvn clean test package
-
-s3cmd put suripu-research/suripu-research.prod.yml s3://hello-research-deploy/suripu-research.prod.yml
-s3cmd put suripu-research/target/suripu-research-*-SNAPSHOT.jar s3://hello-research-deploy/suripu-research.jar
