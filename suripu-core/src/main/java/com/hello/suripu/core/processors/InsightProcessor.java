2f1741bfe7d7844099672b71e63bdab88f3f268c
Jingyun <jingyunfan@gmail.com>
2015-12-17 18:34:13 -0800

    force insight categories not to repeat more than twice every 2 weeks

--- suripu-core/src/main/java/com/hello/suripu/core/processors/InsightProcessor.java
+++ suripu-core/src/main/java/com/hello/suripu/core/processors/InsightProcessor.java
@@ -415,8 +415,8 @@ public class InsightProcessor {
     }
 
     private Set<InsightCard.Category> getRecentInsightsCategories(final Long accountId) {
-        // get all insights from the past week
-        final DateTime aWeekAgo = DateTime.now(DateTimeZone.UTC).minusDays(6);
+        // get all insights from the two weeks
+        final DateTime aWeekAgo = DateTime.now(DateTimeZone.UTC).minusDays(14);
         final Boolean chronological = true;
 
         final List<InsightCard> cards = this.insightsDAODynamoDB.getInsightsByDate(accountId, aWeekAgo, chronological, RECENT_DAYS);
