c6e6424cd0e2d220650b4a8928eb3e16e3822528
Jingyun <jingyunfan@gmail.com>
2016-01-11 11:58:04 -0800

    tweak for layout change

--- suripu-core/src/main/java/com/hello/suripu/core/models/Insights/Message/PartnerMotionMsgEN.java
+++ suripu-core/src/main/java/com/hello/suripu/core/models/Insights/Message/PartnerMotionMsgEN.java
@@ -8,19 +8,19 @@ public class PartnerMotionMsgEN {
 
     public static Text getBadPartner(final Integer percentage) {
         return new Text("The partner",
-                String.format("Your partner moves %d%% more than you. ", percentage) +
-                "Read on about your partner's impact on your sleep.");
+                String.format("Last night, your partner moved %d%% more than you. ", percentage) +
+                "This and other factors can affect the quality of your sleep.");
     }
 
     public static Text getEgalitarian() {
         return new Text("Match made in heaven",
-                "You move about the same amount as your partner. Read on about the link between your partner and your sleep.");
+                "Last night, you moved about the same amount as your partner. Your partner can affect the quality of your sleep.");
     }
 
     public static Text getBadMe(final Integer percentage) {
         return new Text("It's me, not you",
-                String.format("You move %d%% more than your partner. ", percentage) +
-                "Read on about your impact on your partner's sleep.");
+                String.format("Last night, you moved %d%% more than your partner. ", percentage) +
+                "This and other factors can affect the quality of your sleep.");
     }
 
 }
