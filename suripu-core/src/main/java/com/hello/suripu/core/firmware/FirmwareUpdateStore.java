4a76c1b8239c52e2faa88e7a949eb5f6aad50228
Josef Norgan <josef@sayhello.com>
2016-01-15 15:18:24 -0800

    Fixed bug where new firmware versions stored in build_info.txt are dec not hex

--- suripu-core/src/main/java/com/hello/suripu/core/firmware/FirmwareUpdateStore.java
+++ suripu-core/src/main/java/com/hello/suripu/core/firmware/FirmwareUpdateStore.java
@@ -13,6 +13,7 @@ import com.google.common.base.Optional;
 import com.google.common.base.Splitter;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import com.google.common.collect.Maps;
 import com.google.common.collect.Ordering;
 import com.google.common.io.CharStreams;
 import com.google.protobuf.ByteString;
@@ -143,10 +144,27 @@ public class FirmwareUpdateStore {
                 }
 
                 final Iterable<String> strings = Splitter.on("\n").split(text);
-                final String firstLine = strings.iterator().next();
-                String[] parts = firstLine.split(":");
+                final Map<String, String> buildInfo = Maps.newHashMap();
+
+                for (final String line : strings) {
+                    if (line.contains(":")) {
+                        final String[] parts = line.split(":");
+                        buildInfo.put(parts[0].trim(), parts[1].trim());
+                    }
+                }
+
+                if (buildInfo.get("version").isEmpty()) {
+                    return emptyPair;
+                }
+
                 try {
-                    firmwareVersion = Integer.parseInt(parts[1].trim(), 16);
+                    if (buildInfo.get("version").equals(buildInfo.get("travis_build_number"))) {
+                        //version number is already dec
+                        firmwareVersion = Integer.parseInt(buildInfo.get("version"), 10);
+                    } else {
+                        //Assume version is stored as hex
+                        firmwareVersion = Integer.parseInt(buildInfo.get("version"), 16);
+                    }
                 } catch (NumberFormatException nfe) {
                     LOGGER.error("Firmware version in {} is not a valid firmware version. Ignoring this update", objectKey);
                     return emptyPair;
