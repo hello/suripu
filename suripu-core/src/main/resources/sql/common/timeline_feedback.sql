675aba5321a643498606341818c49c58561eb04b
kingshy <kingshy@sayhello.com>
2016-01-19 17:49:29 -0800

    update timeline_feedback table

--- suripu-core/src/main/resources/sql/common/timeline_feedback.sql
+++ suripu-core/src/main/resources/sql/common/timeline_feedback.sql
@@ -28,7 +28,26 @@ CREATE TABLE timeline_feedback(
 -- 2015-12-14 Jake adds a column to determine if feedback was "correct" time or not.
 ALTER TABLE timeline_feedback ADD COLUMN is_correct boolean DEFAULT TRUE;
 
--- UPDATES TO timeline_feedback TABLE 2016-01-15
+-- UPDATES TO timeline_feedback TABLE 2016-01-19
 --on purpose, default value is null, we are going to go in an back-populate
-ALTER TABLE timeline_feedback ADD COLUMN adjustment_delta_millis INTEGER;
-Status API Training Shop Blog About Pricing
+ALTER TABLE timeline_feedback ADD COLUMN adjustment_delta_minutes INTEGER;
+
+-- UPDATE NEW COLUMN IN TABLE
+UPDATE timeline_feedback
+  SET adjustment_delta_minutes = q3.corrected_time FROM
+    (SELECT id, CASE
+      WHEN timediff > 720 THEN timediff - 1440
+      WHEN timediff < -720 THEN 1440 + timediff
+      ELSE timediff
+      END as corrected_time
+      FROM
+        (SELECT q1.id,(extract(epoch from t2) - extract(epoch from t1))/60 timediff FROM
+          (SELECT id,
+            to_timestamp(concat('1970-01-01 ',new_time),'YYY-MM-DD HH24:mi')::timestamp without time zone t2,
+            to_timestamp(concat('1970-01-01 ',old_time),'YYY-MM-DD HH24:mi')::timestamp without timezone t1
+            FROM timeline_feedback
+          ) q1
+        ) q2
+      ) q3
+  WHERE timeline_feedback.id = q3.id;
+
