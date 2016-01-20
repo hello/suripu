16d9d66b6b7e90d77e935d9e016d052fd5155b82
Benjamin Joseph <benjo@sayhello.com>
2016-01-19 17:24:47 -0800

    feedback DAO needed to be updated to reflect new column

--- suripu-core/src/main/java/com/hello/suripu/core/db/FeedbackDAO.java
+++ suripu-core/src/main/java/com/hello/suripu/core/db/FeedbackDAO.java
@@ -10,39 +10,29 @@ import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
 
 @RegisterMapper(TimelineFeedbackMapper.class)
 public abstract class FeedbackDAO extends FeedbackReadDAO {
-
-    @SqlUpdate("INSERT INTO sleep_feedback (account_id, day, hour, correct) VALUES(:account_id, :day, :hour, :correct)")
-    abstract void insert(@Bind("account_id") final Long accountId, @Bind("day") final String day, @Bind("hour") String hour, @Bind("correct") final Boolean correct);
-
+    
     //update timeline_feedback set new_time='07:15' where account_id=1012 and date_of$
 
     /* update if the passed object's old_time,account_id,night,type == database object's new_time,account_id,night,type
     *  So, basically, if passed object's old_time matches the DB entry's new_time, replace the DB entry's new_time with the passed object's new_time
     * */
     @SqlUpdate("UPDATE timeline_feedback " +
-            "SET new_time=:new_time,created=now(), is_correct=:is_correct " +
+            "SET new_time=:new_time,created=now(), is_correct=:is_correct, adjustment_delta_minutes=:adjustment_delta_minutes" +
             "WHERE account_id=:account_id AND date_of_night=:date_of_night AND event_type=:event_type AND new_time=:old_time")
     abstract int updateExistingFeedbackByAccountAndDateAndTime(@Bind("account_id") final Long accountId,@BindTimelineFeedback final TimelineFeedback timelineFeedback);
 
     /* if event type, date, and account id match, update it   */
     @SqlUpdate("UPDATE timeline_feedback " +
-            "SET new_time=:new_time, old_time=:old_time, is_correct=:is_correct, created=now() " +
+            "SET new_time=:new_time, old_time=:old_time, is_correct=:is_correct, created=now(), adjustment_delta_minutes=:adjustment_delta_minutes" +
             "WHERE account_id=:account_id AND date_of_night=:date_of_night AND event_type=:event_type")
     public abstract int updateDuplicate(@Bind("account_id") final Long accountId,@BindTimelineFeedback final TimelineFeedback timelineFeedback);
 
-    @SqlUpdate("INSERT INTO timeline_feedback (account_id, date_of_night, old_time, new_time, event_type, created, is_correct) VALUES(:account_id, :date_of_night, :old_time, :new_time, :event_type, now(), :is_correct)")
+    @SqlUpdate("INSERT INTO timeline_feedback (account_id, date_of_night, old_time, new_time, event_type, created, is_correct,adjustment_delta_minutes) VALUES(:account_id, :date_of_night, :old_time, :new_time, :event_type, now(), :is_correct,:adjustment_delta_minutes)")
     public abstract void insertNewTimelineFeedback(@Bind("account_id") final Long accountId, @BindTimelineFeedback final TimelineFeedback timelineFeedback);
 
     /* Update new_time=old_time by Row ID   */
-    @SqlUpdate("UPDATE timeline_feedback SET new_time=old_time WHERE id=:id")
+    @SqlUpdate("UPDATE timeline_feedback SET new_time=old_time, adjustment_delta_minutes=0 WHERE id=:id")
     public abstract int undoFeedbackUpdate(@Bind("id") final Long id);
-
-    public void insert(SleepFeedback feedback) {
-        if(feedback.accountId.isPresent()) {
-            insert(feedback.accountId.get(), feedback.day, feedback.hour, feedback.correct);
-        }
-
-    }
 
     public void insertTimelineFeedback(final long accountId,final TimelineFeedback feedback) {
 
