5476d7c63a32da82e2e5bfdf6651dce1aa1d4ee6
Jingyun <jingyunfan@gmail.com>
2016-03-09 15:51:16 -0800

    sleep score insight short text

---
+++ suripu-core/src/main/java/com/hello/suripu/core/models/Insights/Message/SleepScoreMsgEN.java
@@ -0,0 +1,11 @@
+package com.hello.suripu.core.models.Insights.Message;
+
+/**
+ * Created by jyfan on 3/9/16.
+ */
+public class SleepScoreMsgEN {
+    public static Text getSleepScoreMarketing() {
+        return new Text("sleep score marketing short",
+                "Your Sleep Score gives you an at-a-glance indicator of your sleep quality for a given night.");
+    }
+}
--- suripu-core/src/main/java/com/hello/suripu/core/processors/insights/SleepScore.java
+++ suripu-core/src/main/java/com/hello/suripu/core/processors/insights/SleepScore.java
@@ -2,6 +2,7 @@ package com.hello.suripu.core.processors.insights;
 
 import com.google.common.base.Optional;
 import com.hello.suripu.core.models.Insights.InsightCard;
+import com.hello.suripu.core.models.Insights.Message.SleepScoreMsgEN;
 import com.hello.suripu.core.models.Insights.Message.Text;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
@@ -11,8 +12,8 @@ import org.joda.time.DateTimeZone;
  */
 public class SleepScore {
     public static Optional<InsightCard> getMarketingInsights(final Long accountId) {
-        final Text text =  new Text("Sleep score marketing", "Sleep score marketing short text. CHANGEME");
-        final String categoryName = "Sleep Score CHANGEME";
+        final Text text = SleepScoreMsgEN.getSleepScoreMarketing();
+        final String categoryName = "Sleep Score";
         return Optional.of(new InsightCard(accountId, text.title, text.message, InsightCard.Category.SLEEP_SCORE, InsightCard.TimePeriod.NONE, DateTime.now(DateTimeZone.UTC), categoryName, InsightCard.InsightType.BASIC));
     }
 }
