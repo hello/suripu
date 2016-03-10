87ebd0a7eca54816d1c9e6cb5396c8011d73c487
jakepic1 <jake@sayhello.com>
2016-03-09 16:38:01 -0800

    FileInfoDAO

---
+++ suripu-core/src/main/java/com/hello/suripu/core/models/FileInfo.java
@@ -0,0 +1,114 @@
+package com.hello.suripu.core.models;
+
+/**
+ * Created by jakepiccolo on 3/9/16.
+ */
+public class FileInfo {
+
+    public enum FileType {
+        SLEEP_SOUND,
+        ALARM
+    }
+
+    public final Long id;
+
+    /**
+     * Sense File path
+     */
+    public final String path;
+
+    public final FileType fileType;
+
+    public final String sha;
+
+    public final String uri;
+
+    public final String previewUri;
+
+    public final String name;
+
+    public final Long firmwareVersion;
+
+    final Boolean isPublic;
+
+    private FileInfo(Builder builder) {
+        id = builder.id;
+        path = builder.path;
+        fileType = builder.fileType;
+        sha = builder.sha;
+        uri = builder.uri;
+        previewUri = builder.previewUri;
+        name = builder.name;
+        firmwareVersion = builder.firmwareVersion;
+        isPublic = builder.isPublic;
+    }
+
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+
+        private Long id;
+        private String path;
+        private FileType fileType;
+        private String sha;
+        private String uri;
+        private String previewUri;
+        private String name;
+        private Long firmwareVersion;
+        private Boolean isPublic;
+
+        private Builder() {
+        }
+
+        public Builder withId(final Long val) {
+            id = val;
+            return this;
+        }
+
+        public Builder withPath(String val) {
+            path = val;
+            return this;
+        }
+
+        public Builder withFileType(FileType val) {
+            fileType = val;
+            return this;
+        }
+
+        public Builder withSha(String val) {
+            sha = val;
+            return this;
+        }
+
+        public Builder withUri(String val) {
+            uri = val;
+            return this;
+        }
+
+        public Builder withPreviewUri(String val) {
+            previewUri = val;
+            return this;
+        }
+
+        public Builder withName(String val) {
+            name = val;
+            return this;
+        }
+
+        public Builder withFirmwareVersion(Long val) {
+            firmwareVersion = val;
+            return this;
+        }
+
+        public Builder withIsPublic(Boolean val) {
+            isPublic = val;
+            return this;
+        }
+
+        public FileInfo build() {
+            return new FileInfo(this);
+        }
+    }
+}
