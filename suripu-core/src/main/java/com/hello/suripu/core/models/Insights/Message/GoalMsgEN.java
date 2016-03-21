76714613a27e3990114c19b0b69c6cd2c5db5635
Jingyun <jingyunfan@gmail.com>
2016-03-21 16:51:42 -0700

    real isight short title

--- suripu-core/src/main/java/com/hello/suripu/core/models/Insights/Message/GoalMsgEN.java
+++ suripu-core/src/main/java/com/hello/suripu/core/models/Insights/Message/GoalMsgEN.java
@@ -7,7 +7,7 @@ public class GoalMsgEN {
 
     public final static String GOAL_WEEKLONG_CATEGORY_NAME = "Weeklong Goal";
 
-    public final static String GOAL_GO_OUTSIDE_TITLE = "goal go outside short";
+    public final static String GOAL_GO_OUTSIDE_TITLE = "Go Outside";
     public final static String GOAL_GO_OUTSIDE_MSG = "This week, try to spend 15 minutes outside every day. For example, take a short walk after lunch.";
 
 }
