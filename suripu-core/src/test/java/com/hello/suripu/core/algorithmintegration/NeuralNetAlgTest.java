7de42bd53fc6ac757a4808f3e5bb07573f37fd93
Benjamin Joseph <benjo@sayhello.com>
2016-04-11 20:46:38 -0700

    test throws exception

--- suripu-core/src/test/java/com/hello/suripu/core/algorithmintegration/NeuralNetAlgTest.java
+++ suripu-core/src/test/java/com/hello/suripu/core/algorithmintegration/NeuralNetAlgTest.java
@@ -20,42 +20,28 @@ public class NeuralNetAlgTest extends NeuralNetAlgorithm {
 
     public NeuralNetAlgTest() {
         super(null);
-
     }
 
 
 
     @Test
     //spot sanity check, just make sure it produces a matrix without any exceptions
-    public void testGetSensorData() {
+    public void testGetSensorData() throws Exception {
         DateTime date = DateTimeUtil.ymdStringToDateTime("2015-09-01");
         DateTime startTime = date.withHourOfDay(18);
         DateTime endTime = startTime.plusHours(16);
 
 
-        /*
-
-          public OneDaysSensorData(final AllSensorSampleList allSensorSampleList,
-                             final ImmutableList<TrackerMotion> trackerMotions, final ImmutableList<TrackerMotion> partnerMotions,
-                             final ImmutableList<TimelineFeedback> feedbackList,
-                             final ImmutableList<TrackerMotion> originalTrackerMotions, final ImmutableList<TrackerMotion> originalPartnerTrackerMotions,
-                             final DateTime date, final DateTime startTimeLocalUTC, final DateTime endTimeLocalUTC, final DateTime currentTimeUTC,
-                             final int timezoneOffsetMillis) {
-         */
         AllSensorSampleList senseData = OnlineHmmTest.getTypicalDayOfSense(startTime,endTime,0);
         ImmutableList<TrackerMotion> pillData = OnlineHmmTest.getTypicalDayOfPill(startTime.minusHours(4),endTime.plusHours(4),0);
         final ImmutableList<TimelineFeedback> emptyFeedback = ImmutableList.copyOf(Lists.<TimelineFeedback>newArrayList());
         final OneDaysSensorData oneDaysSensorData = new OneDaysSensorData(senseData,pillData,pillData,emptyFeedback,pillData,pillData,date,startTime,endTime,endTime,0);
 
-        try {
-            final double[][] neuralNetInput = getSensorData(oneDaysSensorData);
+        final double[][] neuralNetInput = getSensorData(oneDaysSensorData);
 
-            TestCase.assertEquals(SensorIndices.MAX_NUM_INDICES,neuralNetInput.length);
-            TestCase.assertEquals(961,neuralNetInput[0].length);
+        TestCase.assertEquals(SensorIndices.MAX_NUM_INDICES,neuralNetInput.length);
+        TestCase.assertEquals(961,neuralNetInput[0].length);
 
-        }
-        catch (Exception e) {
-            TestCase.assertTrue(false);
-        }
+
     }
 }
