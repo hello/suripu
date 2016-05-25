e28447550906fe5795ab40cc811e02e398f64a0b
kingshy <kingshy@sayhello.com>
2016-05-25 10:53:50 -0700

    one more test

--- suripu-core/src/test/java/com/hello/suripu/core/db/SleepScoreParametersDynamoDBIT.java
+++ suripu-core/src/test/java/com/hello/suripu/core/db/SleepScoreParametersDynamoDBIT.java
@@ -35,11 +35,40 @@ public class SleepScoreParametersDynamoDBIT extends DynamoDBIT<SleepScoreParamet
 
         SleepScoreParameters retrievedParameter = dao.getSleepScoreParametersByDate(accoundId, dateTime);
         assertThat(retrievedParameter.durationThreshold, is(100));
+        assertThat(retrievedParameter.accountId, is(accoundId));
 
         insertResult = dao.upsertSleepScoreParameters(accoundId, new SleepScoreParameters(accoundId, dateTime, 200));
         assertThat(insertResult, is(true));
 
         retrievedParameter = dao.getSleepScoreParametersByDate(accoundId, dateTime);
         assertThat(retrievedParameter.durationThreshold, is(200));
+    }
+
+    @Test
+    public void testGetLatestParameters() throws Exception {
+        final Long accoundId = 1L;
+        final DateTime dateTime = DateTime.now(DateTimeZone.UTC);
+
+        Boolean insertResult;
+        insertResult = dao.upsertSleepScoreParameters(accoundId, new SleepScoreParameters(accoundId, dateTime.minusDays(2), 500));
+        assertThat(insertResult, is(true));
+
+        insertResult = dao.upsertSleepScoreParameters(accoundId, new SleepScoreParameters(accoundId, dateTime.minusDays(10), 1000));
+        assertThat(insertResult, is(true));
+
+        // this should get the 1000 threshold
+        SleepScoreParameters retrievedParameter = dao.getSleepScoreParametersByDate(accoundId, dateTime.minusDays(5));
+        assertThat(retrievedParameter.durationThreshold, is(1000));
+        assertThat(retrievedParameter.accountId, is(accoundId));
+
+        // get 500 threshold
+        retrievedParameter = dao.getSleepScoreParametersByDate(accoundId, dateTime.minusDays(1));
+        assertThat(retrievedParameter.durationThreshold, is(500));
+        assertThat(retrievedParameter.accountId, is(accoundId));
+
+        // no data for this date, should get default
+        retrievedParameter = dao.getSleepScoreParametersByDate(accoundId, dateTime.minusDays(20));
+        assertThat(retrievedParameter.durationThreshold, is(SleepScoreParameters.MISSING_DURATION_THRESHOLD));
+
     }
 }
