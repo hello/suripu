2200fcf61f2d62f8509f89deefc07877fb2bf998
kingshy <kingshy@sayhello.com>
2016-05-31 16:54:31 -0700

    batch get sleep-stats with filters

--- suripu-core/src/main/java/com/hello/suripu/core/db/SleepStatsDAO.java
+++ suripu-core/src/main/java/com/hello/suripu/core/db/SleepStatsDAO.java
@@ -7,6 +7,8 @@ import com.hello.suripu.core.models.SleepScore;
 import com.hello.suripu.core.models.SleepStats;
 import org.joda.time.DateTime;
 
+import java.util.Set;
+
 /**
  * Created by benjo on 1/21/16.
  */
@@ -20,4 +22,6 @@ public interface SleepStatsDAO {
     Optional<AggregateSleepStats> getSingleStat(Long accountId, String date);
 
     ImmutableList<AggregateSleepStats> getBatchStats(Long accountId, String startDate, String endDate);
+
+    ImmutableList<AggregateSleepStats> getBatchStatsFilterByDates(Long accountId, String startDate, String endDate, Set<String> filterDates);
 }
--- suripu-core/src/test/java/com/hello/suripu/core/processors/TimelineProcessorTest.java
+++ suripu-core/src/test/java/com/hello/suripu/core/processors/TimelineProcessorTest.java
@@ -175,6 +175,11 @@ public class TimelineProcessorTest {
         public ImmutableList<AggregateSleepStats> getBatchStats(Long accountId, String startDate, String endDate) {
             return null;
         }
+
+        @Override
+        public ImmutableList<AggregateSleepStats> getBatchStatsFilterByDates(Long accountId,  String startDate, String endDate, Set<String> filterDates) {
+            return null;
+        }
     };
 
     final SenseColorDAO senseColorDAO = new SenseColorDAO() {
