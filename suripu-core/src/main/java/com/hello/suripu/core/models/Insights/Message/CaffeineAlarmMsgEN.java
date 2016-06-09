73dc62a501c6b90ffc7b83d0271fab61ea8e5897
Jingyun <jingyunfan@gmail.com>
2016-06-08 18:14:04 -0700

    copy change prishil

--- suripu-core/src/main/java/com/hello/suripu/core/models/Insights/Message/CaffeineAlarmMsgEN.java
+++ suripu-core/src/main/java/com/hello/suripu/core/models/Insights/Message/CaffeineAlarmMsgEN.java
@@ -5,7 +5,7 @@ package com.hello.suripu.core.models.Insights.Message;
  */
 public class CaffeineAlarmMsgEN {
     public static Text getCaffeineAlarmMessage(final String sleepTime, final String coffeeTime) {
-        final String messageBody = String.format("Since you usually go to bed at %s, try not to drink coffee after %s in order to minimize the effect of caffeine on your sleep.", sleepTime, coffeeTime);
+        final String messageBody = String.format("Since you usually go to bed at around %s, try not to drink coffee after %s in order to minimize the effect of caffeine on your sleep.", sleepTime, coffeeTime);
         return new Text("Coffee Time", messageBody);
     }
 
--- suripu-core/src/main/java/com/hello/suripu/core/models/Insights/Message/SleepAlarmMsgEN.java
+++ suripu-core/src/main/java/com/hello/suripu/core/models/Insights/Message/SleepAlarmMsgEN.java
@@ -8,7 +8,7 @@ public class SleepAlarmMsgEN {
     public static Text getSleepAlarmMessage(final String wakeTime, final int sleepDurationMinutes, final String preSleepTime, final String sleepTime) {
         final float sleepDurationHr = (float) sleepDurationMinutes/60;
 
-        final String messageBody = String.format("You typically wake up at %s. If you want to get %.1f hours of sleep, you should start preparing for bed at %s, and plan to sleep by %s.", wakeTime, sleepDurationHr, preSleepTime, sleepTime);
+        final String messageBody = String.format("You typically wake up at around %s. If you want to get %.1f hours of sleep, you should start preparing for bed at %s, and plan to sleep by %s.", wakeTime, sleepDurationHr, preSleepTime, sleepTime);
         return new Text("Bed Time", messageBody);
     }
 
