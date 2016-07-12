7a308b4d6245d91fa202584389ce54ada89b9d2f
Jarred Heinrich <jarred@sayhello.com>
2016-07-12 16:10:03 -0700

    reopening question_ask_time_revision

--- suripu-core/src/main/java/com/hello/suripu/core/processors/FeatureFlippedProcessor.java
+++ suripu-core/src/main/java/com/hello/suripu/core/processors/FeatureFlippedProcessor.java
@@ -186,4 +186,7 @@ public class FeatureFlippedProcessor {
         return featureFlipper.userFeatureActive(FeatureFlipper.SLEEP_SCORE_V4, accountId, Collections.EMPTY_LIST);
     }
 
+    protected Boolean useQuestionAskTime(final Long accountId){
+        return featureFlipper.userFeatureActive(FeatureFlipper.QUESTION_ASK_TIME_ENABLED, accountId, Collections.EMPTY_LIST);
+    }
 }
