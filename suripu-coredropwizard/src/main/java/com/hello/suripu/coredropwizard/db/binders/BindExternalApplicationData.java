ea124d0a27a934597fb53b6b8aa1cd5b6eef3e7a
Josef Norgan <josef@sayhello.com>
2016-09-14 12:56:30 -0700

    Our Postgres servers aren't running > 9.4, no jsonb data type

--- suripu-coredropwizard/src/main/java/com/hello/suripu/coredropwizard/db/binders/BindExternalApplicationData.java
+++ suripu-coredropwizard/src/main/java/com/hello/suripu/coredropwizard/db/binders/BindExternalApplicationData.java
@@ -2,7 +2,6 @@ package com.hello.suripu.coredropwizard.db.binders;
 
 import com.hello.suripu.coredropwizard.oauth.ExternalApplicationData;
 
-import org.postgresql.util.PGobject;
 import org.skife.jdbi.v2.SQLStatement;
 import org.skife.jdbi.v2.sqlobject.Binder;
 import org.skife.jdbi.v2.sqlobject.BinderFactory;
@@ -13,7 +12,6 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.sql.SQLException;
 
 @BindingAnnotation(BindExternalApplicationData.JsonBinderFactory.class)
 @Retention(RetentionPolicy.RUNTIME)
@@ -26,16 +24,9 @@ public @interface BindExternalApplicationData {
             return new Binder<BindExternalApplicationData, ExternalApplicationData>() {
                 @Override
                 public void bind(SQLStatement q, BindExternalApplicationData bind, ExternalApplicationData arg) {
-                    try {
-                        PGobject data = new PGobject();
-                        data.setType("jsonb");
-                        data.setValue(arg.data);
-                        q.bind("data", data);
+                        q.bind("data", arg.data);
                         q.bind("device_id", arg.deviceId);
                         q.bind("app_id", arg.appId);
-                    } catch (SQLException ex) {
-                        throw new IllegalStateException("Error Binding JSON",ex);
-                    }
                 }
             };
         }
