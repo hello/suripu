cff4054196fc25e7e4b710f2d8756befd9e55bb0
Jarred Heinrich <jarred@sayhello.com>
2016-09-23 16:16:10 -0700

    changed message from minutes to hours

--- suripu-core/src/main/java/com/hello/suripu/core/models/Insights/Message/SleepDeprivationMsgEN.java
+++ suripu-core/src/main/java/com/hello/suripu/core/models/Insights/Message/SleepDeprivationMsgEN.java
@@ -6,10 +6,15 @@ package com.hello.suripu.core.models.Insights.Message;
 public class SleepDeprivationMsgEN {
 
     public static Text getSleepDeprivationMessage(final int idealDuration, final int avgSleepDebtDuration) {
-
+        final float avgSleepDebtHour = Math.round(avgSleepDebtDuration / 30) / 2;
+        if (avgSleepDebtHour == 1) {
+            return new Text("The importance of sleep",
+                    String.format("For the last four nights, you got less than your ideal %d hours of sleep. ", idealDuration)
+                            + String.format("During those nights, you slept %.1f hour less than your nightly average.", avgSleepDebtHour));
+        }
         return new Text("The importance of sleep",
                 String.format("For the last four nights, you got less than your ideal %d hours of sleep. ", idealDuration)
-                        + String.format("During those nights, you slept %d minutes less than your nightly average.", avgSleepDebtDuration));
+                        + String.format("During those nights, you slept %.1f hours less than your nightly average.", avgSleepDebtHour));
     }
 
 }
