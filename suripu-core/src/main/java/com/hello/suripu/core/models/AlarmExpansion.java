434da29f48dc5761af0a5c8287b06e02e5ebf9ef
Josef Norgan <josef@sayhello.com>
2016-10-11 10:46:58 -0700

    Early rough commit

---
+++ suripu-core/src/main/java/com/hello/suripu/core/models/AlarmExpansion.java
@@ -0,0 +1,41 @@
+package com.hello.suripu.core.models;
+
+import com.google.common.base.Objects;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class AlarmExpansion {
+
+    @JsonProperty("id")
+    public final long id;
+
+    @JsonProperty("enable")
+    public final Boolean enable;
+
+    @JsonCreator
+    public AlarmExpansion(@JsonProperty("id") long id,
+                          @JsonProperty("enable") final Boolean enable){
+        this.id = id;
+        this.enable = enable;
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hashCode(this.id);
+    }
+
+    @Override
+    public boolean equals(final Object other){
+        if (other == null){
+            return false;
+        }
+
+        if (getClass() != other.getClass()){
+            return false;
+        }
+
+        final AlarmExpansion sound = (AlarmExpansion) other;
+        return Objects.equal(sound.id, this.id);
+    }
+}
