5b0af8a2c46add6012cc6e61790e01e9e1f0c352
Jarred Heinrich <jarred@sayhello.com>
2016-11-08 16:40:00 -0800

    fixed number formatting

--- suripu-core/src/main/java/com/hello/suripu/core/models/Insights/Message/SleepDeprivationMsgEN.java
+++ suripu-core/src/main/java/com/hello/suripu/core/models/Insights/Message/SleepDeprivationMsgEN.java
@@ -1,5 +1,7 @@
 package com.hello.suripu.core.models.Insights.Message;
 
+import java.text.DecimalFormat;
+
 /**
  * Created by jarredheinrich on 7/8/16.
  */
@@ -7,14 +9,16 @@ public class SleepDeprivationMsgEN {
 
     public static Text getSleepDeprivationMessage(final int idealDuration, final int avgSleepDebtDuration) {
         final float avgSleepDebtHour = ((float) (avgSleepDebtDuration / 30) ) / 2.0f;
+        final DecimalFormat df = new DecimalFormat("0.###");
+        String plural = "s";
         if (avgSleepDebtHour == 1.0f) {
+            plural = "";
+        }
+
             return new Text("The importance of sleep",
                     String.format("For the last four nights, you got less than your ideal %d hours of sleep. ", idealDuration)
-                            + String.format("During those nights, you slept %.0f hour less than your nightly average.", avgSleepDebtHour));
+                            + String.format("During those nights, you slept %s hour%s less than your nightly average.", df.format(avgSleepDebtHour), plural));
         }
-        return new Text("The importance of sleep",
-                String.format("For the last four nights, you got less than your ideal %d hours of sleep. ", idealDuration)
-                        + String.format("During those nights, you slept %.1f hours less than your nightly average.", avgSleepDebtHour));
-    }
+
 
 }
