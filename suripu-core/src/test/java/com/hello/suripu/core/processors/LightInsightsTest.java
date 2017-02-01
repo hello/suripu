a8c3b52c5845fb507651343cdaa9e4d4ab6af82e
Jingyun <jingyunfan@gmail.com>
2017-01-31 17:20:36 -0800

    update light insight, test

--- suripu-core/src/test/java/com/hello/suripu/core/processors/LightInsightsTest.java
+++ suripu-core/src/test/java/com/hello/suripu/core/processors/LightInsightsTest.java
@@ -7,6 +7,7 @@ import com.hello.suripu.core.models.Insights.InsightCard;
 import com.hello.suripu.core.models.Insights.Message.LightMsgEN;
 import com.hello.suripu.core.processors.insights.LightData;
 import com.hello.suripu.core.processors.insights.Lights;
+import com.hello.suripu.core.sense.data.SenseOneFiveExtraData;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.junit.Test;
@@ -26,7 +27,7 @@ public class LightInsightsTest {
     public void testLightGood() {
         final Long accountId = 984L;
         final Long deviceId = 1L;
-        final int light = 2;
+        final int light = 2; //Note this is in lux values, NOT raw light values because 1.0 lux gets converted in DeviceDataDAODynamoDB
 
         final DateTime timestamp = DateTime.now(DateTimeZone.UTC).withHourOfDay(19).withMinuteOfHour(0);
         final int offsetMillis = -28800000;
@@ -35,6 +36,27 @@ public class LightInsightsTest {
         data.add(DeviceData.senseOne(accountId, deviceId, "", 0, 0, 0, 0, 0, 0, 0, light + 1,light + 1, 0, 0, timestamp.withMinuteOfHour(10), offsetMillis, 1, 1, 1, 0, 0, 0, 0));
         data.add(DeviceData.senseOne(accountId, deviceId, "", 0, 0, 0, 0, 0, 0, 0, light - 1,light - 1, 0, 0, timestamp.withMinuteOfHour(30), offsetMillis, 1, 1, 1, 0, 0, 0, 0));
         data.add(DeviceData.senseOne(accountId, deviceId, "", 0, 0, 0, 0, 0, 0, 0, light,light, 0, 0, timestamp.withMinuteOfHour(45), offsetMillis, 1, 1, 1, 0, 0, 0, 0));
+
+        final Optional<InsightCard> insightCardOptional = Lights.processLightData(accountId, data, Optional.of(Device.Color.WHITE), Optional.absent(), new LightData());
+        if (insightCardOptional.isPresent()) {
+            final String expectedTitle = LightMsgEN.getLightDark(0,0).title;
+            assertThat(insightCardOptional.get().title, is(expectedTitle));
+        }
+    }
+
+    @Test
+    public void testLightGood_2() {
+        final Long accountId = 984L;
+        final Long deviceId = 1L;
+        final int light = 2;
+
+        final DateTime timestamp = DateTime.now(DateTimeZone.UTC).withHourOfDay(19).withMinuteOfHour(0);
+        final int offsetMillis = -28800000;
+        final List<DeviceData> data = new ArrayList<>();
+        data.add(DeviceData.senseOneFive(DeviceData.senseOne(accountId, deviceId, "", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, timestamp, offsetMillis, 0, 0, 0, 0, 0, 0, 0), SenseOneFiveExtraData.create(0, 0, 0, "fakergb", 0, 0, light, 0)));
+        data.add(DeviceData.senseOneFive(DeviceData.senseOne(accountId, deviceId, "", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, timestamp.withMinuteOfHour(10), offsetMillis, 1, 1, 1, 0, 0, 0, 0), SenseOneFiveExtraData.create(0, 0, 0, "fakergb", 0, 0, light + 1, 0)));
+        data.add(DeviceData.senseOneFive(DeviceData.senseOne(accountId, deviceId, "", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, timestamp.withMinuteOfHour(30), offsetMillis, 1, 1, 1, 0, 0, 0, 0), SenseOneFiveExtraData.create(0, 0, 0, "fakergb", 0, 0, light + 1, 0)));
+        data.add(DeviceData.senseOneFive(DeviceData.senseOne(accountId, deviceId, "", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, timestamp.withMinuteOfHour(45), offsetMillis, 1, 1, 1, 0, 0, 0, 0), SenseOneFiveExtraData.create(0, 0, 0, "fakergb", 0, 0, 0, 0)));
 
         final Optional<InsightCard> insightCardOptional = Lights.processLightData(accountId, data, Optional.of(Device.Color.WHITE), Optional.absent(), new LightData());
         if (insightCardOptional.isPresent()) {
