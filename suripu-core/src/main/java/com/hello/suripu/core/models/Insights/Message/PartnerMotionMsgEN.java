5e5c8560206a68f4045b42c1a58bd3d7f871e80f
Jingyun Fan <jingyunfan@gmail.com>
2017-02-08 13:52:58 -0800

    match same number of nights (#1880)
    * match same number of nights
    
    * increases cases

--- suripu-core/src/main/java/com/hello/suripu/core/models/Insights/Message/PartnerMotionMsgEN.java
+++ suripu-core/src/main/java/com/hello/suripu/core/models/Insights/Message/PartnerMotionMsgEN.java
@@ -12,6 +12,16 @@ public class PartnerMotionMsgEN {
                 "Your partner's movements can affect the quality of your sleep.");
     }
 
+    public static Text getTwiceBadPartner() {
+        return new Text("The 2X partner",
+                "Last week, your partner moved twice as much as you. Your partner's movements can affect the quality of your sleep.");
+    }
+
+    public static Text getVeryBadPartner() {
+        return new Text("The >2X partner",
+                "Last week, your partner moved more than twice as much as you. Your partner's movements can affect the quality of your sleep.");
+    }
+
     public static Text getEgalitarian() {
         return new Text("Match made in heaven",
                 "Last week, you moved about the same amount as your partner.");
@@ -21,6 +31,16 @@ public class PartnerMotionMsgEN {
         return new Text("It's me, not you",
                 String.format("Last week, you moved %d%% more than your partner. ", percentage) +
                 "Your movements can affect the quality of your partner's sleep.");
+    }
+
+    public static Text getTwiceBadMe() {
+        return new Text("The 2X me",
+                "Last week, you moved twice as much as your partner. Your movements can affect the quality of your partner's sleep.");
+    }
+
+    public static Text getVeryBadMe() {
+        return new Text("The >2X me",
+                "Last week, you moved more than twice as much as your partner. Your movements can affect the quality of your partner's sleep.");
     }
 
 }
