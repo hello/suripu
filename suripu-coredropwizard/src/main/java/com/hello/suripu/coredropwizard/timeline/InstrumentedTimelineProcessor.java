bd986d0661a0f67e78cdd355fedcd6e3a82a0e92
Jarred Heinrich <jarred@sayhello.com>
2017-03-10 15:46:37 -0800

    added log message and comments

--- suripu-coredropwizard/src/main/java/com/hello/suripu/coredropwizard/timeline/InstrumentedTimelineProcessor.java
+++ suripu-coredropwizard/src/main/java/com/hello/suripu/coredropwizard/timeline/InstrumentedTimelineProcessor.java
@@ -736,20 +736,22 @@ public class InstrumentedTimelineProcessor extends FeatureFlippedProcessor {
         Integer sleepScore = computeAndMaybeSaveScore(sensorData.oneDaysTrackerMotion.processedtrackerMotions, sensorData.oneDaysTrackerMotion.filteredOriginalTrackerMotions, numSoundEvents, allSensorSampleList, targetDate, accountId, sleepStats);
 
         //if there is no feedback, we have a "natural" timeline
-        //check if this natural timeline makes sense.  If not, set sleep score to zero.
+        //there should be no case where the sleep duration is less than the min sleep duration at this point - already checked during timeline algorithm stage.
         if (feedbackList.isEmpty() && sleepStats.sleepDurationInMinutes < TimelineSafeguards.MINIMUM_SLEEP_DURATION_MINUTES) {
             LOGGER.warn("action=zeroing-score account_id={} reason=sleep-duration-too-short sleep_duration={}", accountId, sleepStats.sleepDurationInMinutes);
             sleepScore = 0;
         }
 
         boolean isValidSleepScore = sleepScore > 0;
-        if (!isValidSleepScore){
-
-        }
 
         //if there's feedback, sleep score can never be invalid
         if (!feedbackList.isEmpty()) {
             isValidSleepScore = true;
+        }
+
+        if (!isValidSleepScore){
+            //there should not be any invalid sleep scores at this point
+            LOGGER.error("msg=invalid-sleep-score account_id={} date={}", accountId, targetDate);
         }
 
         final String timeLineMessage = timelineUtils.generateMessage(sleepStats, numPartnerMotion, numSoundEvents);
